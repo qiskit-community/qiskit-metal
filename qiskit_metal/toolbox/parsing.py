8b25ddbac6c342f8514868dadfede70447a0d938
Zlatko K. Minev <zminev@gmail.com>
2019-10-29 19:06:15 -0400

    Parse option strings

--- qiskit_metal/toolbox/parsing.py
+++ qiskit_metal/toolbox/parsing.py
@@ -18,14 +18,101 @@ Parsing functions for  Qiskit Metal.
 @date: 2019
 @author: Zlatko K. Minev, Thomas McConkey
 '''
+#TODO: make a local pakcage `parse_units_user` remove from pyEPR dependency
+#TODO: remove HFSS parse to hfss plugin
 
+import ast
 from pyEPR.hfss import parse_units, parse_units_user
 from pyEPR.hfss import unparse_units # not used here, but in imports of this file
-from .. import logger
+from .. import logger, Dict
 
 #########################################################################
 # UNIT and Conversion related
 
+def is_variable_name(test_str : str):
+    """Is the test string a valid name for a variable or not?
+
+    Arguments:
+        test_str {str} -- test string
+
+    Returns:
+        bool
+    """
+    return test_str.isidentifier()
+
+def is_for_ast_eval(test_str : str):
+    """
+    Is the test string a valid list of dict string,
+    such as "[1, 2]", that can be evaluated by ast eval
+
+    Arguments:
+        test_str {str} -- test string
+
+    Returns:
+        bool
+    """
+    return ('[' in test_str and ']' in test_str) or \
+           ('{' in test_str and '}' in test_str)
+
+def is_numeric_possible(test_str:str):
+    """
+    Is the test string a valid possible numerical with /or w/o units.
+
+    Arguments:
+        test_str {str} -- test string
+
+    Returns:
+        bool
+    """
+    return test_str[0].isdigit() or test_str[0] in ['+', '-', '.']
+
+# look into pyparsing
+def parse_value(value : str, variable_dict : dict):
+    """Parse a single string value to correct type and value
+
+    Arguments:
+        value {[str]} -- string to parse
+        variable_dict {[dict]} -- dict pointer of variables
+
+    Return:
+        Parse value: str, float, list, tuple, or ast eval
+    """
+
+    if isinstance(val, str):
+        # remove trailing and leading white spaces in the name
+        val = str(value).strip()
+
+        if is_variable_name(val):
+            # we have a string that could be interpreted as a variable
+            # check if there is such a variable name, else return as string
+            # logger.warning(f'Missing variable {opts[name]} from variable list.\n')
+
+            if val in variable_dict:
+                # Parse the returned value
+                return parse_value(variable_dict[val], variable_dict)
+            else:
+                # Assume it is a string and just return it
+                return val
+
+        elif is_for_ast_eval(val):
+            evaluated = ast.literal_eval(val)
+            if isinstance(evaluated, list):
+                # check if list, parse each element of the list
+                return [parse_value(element, variable_dict) for element in evaluated]
+            elif isinstance(evaluated, list):
+                return Dict({key: parse_value(element, variable_dict) for key, element in evaluated.items()})
+            else:
+                logger.error(f'Unkown is_for_ast_eval\nval={val}\nevaluated={evaluated}')
+                return evaluated
+
+        elif is_numeric_possible(val):
+            return parse_units_user(value)
+
+        else: # assume it is just a string
+            return value
+
+    else: # no parsing needed, it is not a string
+        return value
 
 def parse_options_user(option_dict, parse_names, variable_dict=None):
     """
@@ -60,42 +147,33 @@ def parse_options_user(option_dict, parse_names, variable_dict=None):
         variable_dict = {}
 
     if isinstance(parse_names, str):
-        names = names.split(',')
+        parse_names = parse_names.split(',')
 
     # Do for each name
     # TODO: probably slow with for loop, can think of faster or vectorized method here
-    opts = option_dict  # alias
     res = []
-    for name in names:
+    for name in parse_names:
         name = name.strip()  # remove trailing and leading white spaces in the name
 
         # is the name in the options at all?
-        if not name in opts:
-            logger.warning(f'Missing key {name} from options {opts}.\n')
+        if not name in option_dict:
+            logger.warning(f'Missing key {name} from options {option_dict}. Skipping ...\n')
+            continue
 
-        if isinstance(opts[name], str):
-            if not(opts[name][0].isdigit() or opts[name][0] == '-'):
-                if not opts[name] in variable_dict:
-                    logger.warning(f'Missing variable {opts[name]} from variable list.\n')
-
-                res += [parse_units_user(variable_dict[opts[name]])]
-            else:
-                res += [parse_units_user(opts[name])]
-        else:
-            res += [parse_units_user(opts[name])]
+        # option_dict[name] should be a string
+        res += [ parse_value(option_dict[name], variable_dict) ]
 
     return res
-    # return [parse_units_user(opts[name.strip()]) for name in names]
 
 
-def parse_options_hfss(opts, names):
+def parse_options_hfss(opts, parse_names):
     '''
     To HFSS units.
 
     Parse a list of variable names (or a string of comma delimited ones
     to list of HFSS parsed ones.
     '''
-    if isinstance(names, str):
-        names = names.split(',')
+    if isinstance(parse_names, str):
+        parse_names = parse_names.split(',')
 
-    return [parse_units(opts[name.strip()]) for name in names]
+    return [parse_units(opts[name.strip()]) for name in parse_names]
