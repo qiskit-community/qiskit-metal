e9713cd5553fa92645264dd7bd52c162ba2f908c
Zlatko Minev <zminev@gmail.com>
2020-01-07 20:22:42 -0500

    Added renderer extension and registering to element table

--- qiskit_metal/renderers/renderer_base/renderer_base.py
+++ qiskit_metal/renderers/renderer_base/renderer_base.py
@@ -18,10 +18,10 @@
 """
 
 from ...designs import DesignBase, is_design
-
+from ...elements import ElementTables
 
 class RendererBase():
-    """Abstract base class for all Renderes of Metal designs.
+    """Abstract base class for all Renderers of Metal designs and their compoinents and elements.
 
     Handles:
         designs
@@ -32,7 +32,72 @@ class RendererBase():
             chips
     """
 
+    name = 'base' # overwrite this!
+
+    __loaded_renderers__ = set()
+    __instantiated_renderers__ = dict()
+
+    # overwrite this to add element extensions:  see ELEMENT_COLUMNS
+    # should be dict of dict with keys as element type, which contain (name, dype) pairs
+    # e.g. element_extensions = dict(
+    #            base=dict(color=str, klayer=int),
+    #            path=dict(thickness=float, material=str, perfectE=bool),
+    #            poly=dict(thickness=float, material=str), )
+    element_extensions = dict()
+
+    #TODO: To add: default parameters for the renderer for component element values.
+
+    @classmethod
+    def load(cls):
+        """Load the renderer and register all its extensions.
+        Only performed once.
+
+        Once complete, the rendere is added to the class attribute
+        '__loaded_renderers__' of RendererBase
+
+        Returns:
+            True -- If success, otherwise throws an error.
+        """
+
+        ### Check name
+        name = cls.name
+
+        if name in RendererBase.__loaded_renderers__:
+            print(f'Warning: Renderer name={name}, class={cls} already loaded. Doing nothing.')
+
+        ### Add elemnet extensions
+        # see docstring for RendererBase.element_extensions
+        ElementTables.add_renderer_extension(cls.name, cls.element_extensions)
+
+        #### Add component extensions
+        # to be used in the creation of default params for component elements
+        raise NotImplementedError()
+
+        ### Finish and register offically as ready to use.
+        RendererBase.__loaded_renderers__.add(name)
+
+        return True
+
+    @staticmethod
+    def get_renderer(name:str):
+         """Returns an already loaded and instantiated renderer.
+
+         Arguments:
+             name {[str]} -- [description]
+         """
+         if not name in RendererBase.__loaded_renderers__:
+             print('ERROR: The renderer {name} has not yet been loaded. Please use the load function!')
+
+         if not name in RendererBase.__instantiated_renderers__:
+             print('ERROR: The renderer {name} has not yet been instantiated. Please instantiate the class!')
+
+         return RendererBase.__instantiated_renderers__[name]
+
+
+
     def __init__(self, design: DesignBase, initiate=True):
+        # TODO: check that the renderer has been loaded with load_renderer
+
         assert is_design(design), "Erorr, for the design argument you must provide a\
                                    a child instance of Metal DesignBase class."
 
@@ -41,9 +106,13 @@ class RendererBase():
         self.initiated = False
 
         if initiate:
-            self.initate_renderer()
+            self.initate()
 
-    def initate_renderer(self, re_initiate=False):
+        # Register as an instantiated renderer.
+        RendererBase.__instantiated_renderers__[self.name] = self
+
+
+    def initate(self, re_initiate=False):
         '''
         Call any initiations steps required to be performed a single time before rendering,
         such as conneting to some API or COM, or importing the correct material libraries, etc.
@@ -86,7 +155,7 @@ class RendererBase():
         '''
         Renders all design chips and components.
         '''
-        self.initate_renderer()
+        self.initate()
         self.render_chips()
         self.render_components()
         # ...
@@ -111,15 +180,15 @@ class RendererBase():
         raise NotImplementedError()
 
     def render_element(self, element):
+        raise NotImplementedError()
+        #if isinstance(element, path):
+        #    self.render_element_path(element)
 
-        if isinstance(element, path):
-            self.render_element_path(element)
+        #elif isinstance(element, poly):
+        #    self.render_element_poly(element)
 
-        elif isinstance(element, poly):
-            self.render_element_poly(element)
-
-        else:
-            self.logger.error('RENDERER ERROR: Unkown element {element}')
+        #else:
+        #    self.logger.error('RENDERER ERROR: Unkown element {element}')
 
     def render_element_path(self, path):
         raise NotImplementedError()
