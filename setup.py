23a0f0e252d204c9e0cf56c5b9a425ae7aa38c16
Zlatko K. Minev <zminev@gmail.com>
2020-01-28 14:28:42 -0500

    Added setup.py from v0.2

---
+++ setup.py
@@ -0,0 +1,56 @@
+"""A setuptools based setup module.
+"""
+
+from pathlib import Path
+from setuptools import setup, find_packages
+
+here = Path(__file__).parent.absolute()
+
+# Get the long description from the README file
+with open(here / "README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="qiskit_metal",
+    version="0.1",
+    description="Quantum VLSI and Sims",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # url="https://github.com/Qiskit/qiskit-metal",
+    author="Qiskit Development Team: Zlatko Minev, Thomas McConkey, Will Shanks, and team",
+    author_email="qiskit@qiskit.org",
+    license="Apache 2.0",
+    classifiers=[
+        "Environment :: Console",
+        "License :: OSI Approved :: Apache Software License",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Scientific/Engineering",
+    ],
+    keywords="qiskit sdk quantum",
+    packages=find_packages(),
+    python_requires=">=3.5, <4",
+    install_requires=[
+        "addict",
+        "docutils",
+        "ipython",
+        "matplotlib",
+        "numpy",
+        "pandas",
+        "pint",
+        "pyEPR @ git+https://github.com/zlatko-minev/pyEPR@98dfc5559f40887ec9b74d4bbf9cfcc9229b154c#egg=pyEPR",
+        "Pygments",
+        "PyQt5",
+        "scipy",
+        "Shapely",
+    ],
+    extras_require={"gds": ["gdspy"]},
+)
