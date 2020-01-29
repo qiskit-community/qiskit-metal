a164102b781c9de6549314e7fbf8ddca50e4a9a3
Zlatko K. Minev <zminev@gmail.com>
2020-01-29 00:54:33 -0500

    toolbox: adeded avilable libraries and library objects

--- qiskit_metal/toolbox/__init__.py
+++ qiskit_metal/toolbox/__init__.py
@@ -23,3 +23,25 @@ def monkey_patch(self, func):
     Debug function
     '''
     setattr(self, func.__name__, func.__get__(self, self.__class__))
+
+
+
+from IPython.display import display, Markdown
+display_md = lambda text: display(Markdown(text))
+
+def available_libraries():
+    """ Helper functions to report nicely - skip over this block
+    """
+    import qiskit_metal
+    dir_public = lambda obj: [name for name in dir(obj) if not name.startswith('_')]
+
+    # Print available object categories
+    display_md("**Available object libraries in default library `qiskit_metal.objects`:**")
+    display(dir_public(qiskit_metal.objects))
+
+def library_objects(module):
+    text = f"**The library `{module.__name__}`has objects:**"
+    for name in dir(module):
+        if not name.startswith('_'):
+            text += f"\n - {name}"
+    display_md(text)
