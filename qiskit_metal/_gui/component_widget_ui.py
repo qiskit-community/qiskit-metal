3fe246ffbb9f5beca3c19b97424fdd44648a666d
Zlatko Minev <zminev@gmail.com>
2020-04-08 17:50:14 -0400

    Added rebuild toolbar button

--- qiskit_metal/_gui/component_widget_ui.py
+++ qiskit_metal/_gui/component_widget_ui.py
@@ -72,8 +72,8 @@ class Ui_ComponentWidget(object):
         self.textEdit.setHtml(_translate("ComponentWidget", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt; font-weight:600;\">Here we display the docstring for a component. </span></p></body></html>"))
+"</style></head><body style=\" font-family:\'.SF NS Text\'; font-size:13pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'MS Shell Dlg 2\'; font-size:14pt; font-weight:600;\">Here we display the docstring for a component. </span></p></body></html>"))
         ComponentWidget.setTabText(ComponentWidget.indexOf(self.tabHelp), _translate("ComponentWidget", "Help"))
         ComponentWidget.setTabText(ComponentWidget.indexOf(self.tabSource), _translate("ComponentWidget", "Source"))
 
