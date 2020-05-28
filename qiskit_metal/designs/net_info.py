e6070b86c3224dc863f97cf3c2c82ab36ac8833a
Priti A Shah <Priti.Ashvin.Shah@ibm.com>
2020-05-28 14:01:05 -0400

    Comment-Out code so CI will work.

--- qiskit_metal/designs/net_info.py
+++ qiskit_metal/designs/net_info.py
@@ -31,56 +31,58 @@ class QNet():
         column_names=['net_id', 'component_id', 'pin_name']
         self._net_info = pd.DataFrame(column=column_names)
 
-        #for testing, need values in net_table to test.  
-        self._net_info.append(pd.Series([2, 3, 'qubit'],index=_net_info.columns), ignore_index=True)
-        self._net_info.append(pd.Series([2, 3, 'qubit'],index=_net_info.columns), ignore_index=True)
-        self._net_info.append(pd.Series([2, 3, 'qubit'],index=_net_info.columns), ignore_index=True)
-
-        self._net_table.add((1, 'read1', 3, 'qubit' ))
-        self._net_table.add((1, 'bus1', 4, 'qstart' ))
-        self._net_table.add((2, 'bus2', 5, 'qend' ))
+    def add_for_test(self):
+    #for testing, need values in net_table to test.  
+        self._net_info.append(pd.Series([2, 1, 'qubit'],index=_net_info.columns), ignore_index=True)
+        self._net_info.append(pd.Series([2, 3, 'qstart'],index=_net_info.columns), ignore_index=True)
+        self._net_info.append(pd.Series([1, 1, 'bus1'],index=_net_info.columns), ignore_index=True)
+        self._net_info.append(pd.Series([1, 4, 'qstart'],index=_net_info.columns), ignore_index=True)
+        self._net_info.append(pd.Series([3, 2, 'bus2'],index=_net_info.columns), ignore_index=True)
+        self._net_info.append(pd.Series([3, 5, 'qend'],index=_net_info.columns), ignore_index=True)
 
     def add_net_table(self, item: tuple) -> bool:
+        
         confirm = False
-        (i1, s2, i3, s4) = item
-        assert 
-        # confirm i1 and i3 are ints to represent unique component id.
-        # confirm s2 and s4 are string to represent pins on the component.
+        
+        # (i1, s2, i3, s4) = item
 
-        # i1 and i3 are not same, 
-        # i1 and i3 are ints
-        # s2 and s4 are strings
+        # # confirm i1 and i3 are ints to represent unique component id.
+        # # confirm s2 and s4 are string to represent pins on the component.
 
-        # check to see if net is in the table in reverse order.
-        equal_item = (i3, s4, i1, s2)
+        # # i1 and i3 are not same, 
+        # # i1 and i3 are ints
+        # # s2 and s4 are strings
 
-        if equal_item not in self._net_table:
-            self._net_table.add(item)
-            confirm = True
+        # # check to see if net is in the table in reverse order.
+        # equal_item = (i3, s4, i1, s2)
+
+        # if equal_item not in self._net_table:
+        #     self._net_table.add(item)
+        #     confirm = True
         
         return confirm
 
     def delete_net_table(self, item: tuple) -> bool:
         confirm = False
         
-        (s1, s2, s3, s4) = item
-        equal_item = (s3, s4, s1, s2)
+        # (s1, s2, s3, s4) = item
+        # equal_item = (s3, s4, s1, s2)
 
-        self._net_table.discard(equal_item)
-        self._net_table.discard(item)
-        confirm = True
+        # self._net_table.discard(equal_item)
+        # self._net_table.discard(item)
+        # confirm = True
         
         return confirm
 
     def is_Qomponent_pin_connected(self, component_name: str, port_name: str) -> Tuple[bool, tuple]:
         confirm = False
         aconnector = tuple()
-        comp_1 = ""
-        pin_1 = ""
-        comp_2 = ""
-        pin_2 = ""
-        for item in self._net_table:
-            print (item)
-            pass
+        # comp_1 = ""
+        # pin_1 = ""
+        # comp_2 = ""
+        # pin_2 = ""
+        # for item in self._net_table:
+        #     print (item)
+        #     pass
 
         return (confirm, aconnector)
