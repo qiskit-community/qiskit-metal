e72c8cfc93e8985d88f92b327d7c6480ff7d8e08
Priti A Shah <Priti.Ashvin.Shah@ibm.com>
2020-05-29 14:30:02 -0400

    Just save before switching branches.

--- qiskit_metal/designs/net_info.py
+++ qiskit_metal/designs/net_info.py
@@ -19,7 +19,7 @@
 """
 from typing import Tuple
 import pandas as pd
-import logging
+from .. import logger
 
 
 class QNet():
@@ -28,50 +28,91 @@ class QNet():
 
     def __init__(self):
         """ Hold the net information of all the USED pins within a design."""
-        column_names=['net_id', 'component_id', 'pin_name']
-        self._net_info = pd.DataFrame(column=column_names)
+        self.column_names = ['net_id', 'component_id', 'pin_name']
+        self._net_info = pd.DataFrame(columns=self.column_names)
+        self._qnet_latest_assigned_id = 0
+        self.logger = logger  # type: logging.Logger
+
+    def _get_new_qnet_id(self) -> int:
+        self._qnet_latest_assigned_id += 1
+        return self._qnet_latest_assigned_id
+
+    @property
+    def qnet_latest_assigned_id(self) -> int:
+        '''
+        Return unique number for each net in table.
+        For user of the design class to know the lastest id added to _net_info.
+        '''
+        return self._qnet_latest_assigned_id
 
     def add_for_test(self):
-    #for testing, need values in net_table to test.  
-        self._net_info.append(pd.Series([2, 1, 'qubit'],index=_net_info.columns), ignore_index=True)
-        self._net_info.append(pd.Series([2, 3, 'qstart'],index=_net_info.columns), ignore_index=True)
-        self._net_info.append(pd.Series([1, 1, 'bus1'],index=_net_info.columns), ignore_index=True)
-        self._net_info.append(pd.Series([1, 4, 'qstart'],index=_net_info.columns), ignore_index=True)
-        self._net_info.append(pd.Series([3, 2, 'bus2'],index=_net_info.columns), ignore_index=True)
-        self._net_info.append(pd.Series([3, 5, 'qend'],index=_net_info.columns), ignore_index=True)
+        # for testing, need values in net_table to test.
+        id_local = self._get_new_qnet_id()
+        self._net_info = self._net_info.append(
+            pd.Series([id_local, 1, 'qubit'], index=self._net_info.columns), ignore_index=True)
+        self._net_info = self._net_info.append(
+            pd.Series([id_local, 3, 'qstart'], index=self._net_info.columns), ignore_index=True)
 
-    def add_net_table(self, item: tuple) -> bool:
-        
-        confirm = False
-        
-        # (i1, s2, i3, s4) = item
+        id_local = self._get_new_qnet_id()
+        self._net_info = self._net_info.append(
+            pd.Series([id_local, 1, 'bus1'], index=self._net_info.columns), ignore_index=True)
+        self._net_info = self._net_info.append(
+            pd.Series([id_local, 4, 'qstart'], index=self._net_info.columns), ignore_index=True)
 
-        # # confirm i1 and i3 are ints to represent unique component id.
-        # # confirm s2 and s4 are string to represent pins on the component.
+        id_local = self._get_new_qnet_id()
+        self._net_info = self._net_info.append(
+            pd.Series([id_local, 2, 'bus2'], index=self._net_info.columns), ignore_index=True)
+        self._net_info = self._net_info.append(
+            pd.Series([id_local, 5, 'qend'], index=self._net_info.columns), ignore_index=True)
+        print(self._net_info)
+        pass
 
-        # # i1 and i3 are not same, 
-        # # i1 and i3 are ints
-        # # s2 and s4 are strings
+    def add_to_net_table(self, comp1_id: int, pin1_name: str, comp2_id: int, pin2_name: str) -> int:
+        """Add two entries into the _net_info table.
 
-        # # check to see if net is in the table in reverse order.
-        # equal_item = (i3, s4, i1, s2)
+        Arguments:
+            comp1_id {int} -- [description]
+            pin1_name {str} -- [description]
+            comp2_id {int} -- [description]
+            pint2 {str} -- [description]
 
-        # if equal_item not in self._net_table:
-        #     self._net_table.add(item)
-        #     confirm = True
-        
-        return confirm
+        Returns:
+            int -- 0 if not added to list, otherwise the netid
+        """
+
+        assert isinstance(comp1_id, int), self.logger.error(
+            f'Expected an int, but have {comp1_id}.')
+        assert isinstance(comp2_id, int), self.logger.error(
+            f'Expected an int, but have {comp2_id}.')
+        assert isinstance(pin1_name, str), self.logger.error(
+            f'Expected a string, but have {pin1_name}.')
+        assert isinstance(pin2_name, str), self.logger.error(
+            f'Expected a string, but have {pin2_name}.')
+
+
+
+        net_id = 0   # Zero mean false, the pin was not added to _net_info
+        net_id = self._get_new_qnet_id()
+
+        entry1 = [net_id, comp1_id, pin1_name]
+        entry2 = [net_id, comp2_id, pin2_name]
+        temp_df = pd.DataFrame([entry1, entry2], columns=self.column_names)
+
+        self._net_info = self._net_info.append(temp_df)
+
+        print(self._net_info)
+        return net_id
 
     def delete_net_table(self, item: tuple) -> bool:
         confirm = False
-        
+
         # (s1, s2, s3, s4) = item
         # equal_item = (s3, s4, s1, s2)
 
         # self._net_table.discard(equal_item)
         # self._net_table.discard(item)
         # confirm = True
-        
+
         return confirm
 
     def is_Qomponent_pin_connected(self, component_name: str, port_name: str) -> Tuple[bool, tuple]:
