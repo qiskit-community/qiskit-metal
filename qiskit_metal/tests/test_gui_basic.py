1fadff3e43ed1ca879431b052027bd4e838bded3
JEREMY DRYSDALE <jdrysda@us.ibm.com>
2020-06-05 14:35:33 -0400

    adding gui tests

--- qiskit_metal/tests/test_gui_basic.py
+++ qiskit_metal/tests/test_gui_basic.py
@@ -28,7 +28,8 @@ import unittest
 from qiskit_metal import designs
 from qiskit_metal import MetalGUI
 from qiskit_metal.components.qubits.transmon_pocket import TransmonPocket
-
+import time
+import warnings
 
 
 class TestGUIBasic(unittest.TestCase):
@@ -44,18 +45,7 @@ class TestGUIBasic(unittest.TestCase):
 
         Returns: None
         """
-        print('Setup')
-        # Create an example design
-        #self.design = designs.DesignPlanar()
-
-        #self.q1 = TransmonPocket(self.design, 'Q1', options = dict(pos_x='-1.5mm', pos_y='+0.0mm'))
-        #self.q2 = TransmonPocket(self.design, 'Q2', options = dict(pos_x='+1.5mm', pos_y='+0.0mm'))
-        #self.q3 = TransmonPocket(self.design, 'Q3', options = dict(pos_x='+0.0mm', pos_y='+1.0mm'))
-
-        # Launch GUI
-        #self.gui = MetalGUI(self.design)
-        #TODO: handle check to make sure we set the design correctly
-
+        pass
 
     def tearDown(self):
         """
@@ -67,15 +57,30 @@ class TestGUIBasic(unittest.TestCase):
         """
         pass
 
-    def test_step1(self):
-        #TODO ...
-        print('Step 1')
-        #self.gui.autoscale()
+    def test_gui_01_launch(self):
+        self.design = designs.DesignPlanar()
+        self.gui = MetalGUI(self.design)
+        self.gui.autoscale()
+        
+        
+    def test_gui_refresh_design(self):
+        self.design = designs.DesignPlanar()
+        self.q1 = TransmonPocket(self.design, 'Q1', options = dict(pos_x='-1.5mm', pos_y='+0.0mm'))
+        self.q2 = TransmonPocket(self.design, 'Q2', options = dict(pos_x='+1.5mm', pos_y='+0.0mm'))
+        self.q3 = TransmonPocket(self.design, 'Q3', options = dict(pos_x='+0.0mm', pos_y='+1.0mm'))
+        self.gui = MetalGUI(self.design)
+        self.gui.autoscale()
+        self.gui.refresh_design()
 
-    def test_step2(self):
-        #TODO ...
-        print('Step 2')
-
+    def test_gui_rebuild(self):
+        self.design = designs.DesignPlanar()
+        self.q1 = TransmonPocket(self.design, 'Q1', options = dict(pos_x='-1.5mm', pos_y='+0.0mm'))
+        self.q2 = TransmonPocket(self.design, 'Q2', options = dict(pos_x='+1.5mm', pos_y='+0.0mm'))
+        self.q3 = TransmonPocket(self.design, 'Q3', options = dict(pos_x='+0.0mm', pos_y='+1.0mm'))
+        self.gui = MetalGUI(self.design)
+        self.gui.autoscale()
+        self.gui.rebuild(False)
+        self.gui.rebuild(True)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
