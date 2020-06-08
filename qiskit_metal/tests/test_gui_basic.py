11a14f80e8095e209088cc83e75a5d898a6e00ad
JEREMY DRYSDALE <jdrysda@us.ibm.com>
2020-06-08 13:50:59 -0400

    testing CI

--- qiskit_metal/tests/test_gui_basic.py
+++ qiskit_metal/tests/test_gui_basic.py
@@ -84,7 +84,6 @@ class TestGUIBasic(unittest.TestCase):
         Returns: None
         """
         try:
-            #pass
             design = designs.DesignPlanar()
             q_1 = TransmonPocket(design, 'Q1', options=dict(pos_x='-1.5mm', pos_y='+0.0mm'))
             q_2 = TransmonPocket(design, 'Q2', options=dict(pos_x='+1.5mm', pos_y='+0.0mm'))
@@ -107,15 +106,14 @@ class TestGUIBasic(unittest.TestCase):
         Returns: None
         """
         try:
-            pass
-            #design = designs.DesignPlanar()
-            #q_1 = TransmonPocket(design, 'Q1', options=dict(pos_x='-1.5mm', pos_y='+0.0mm'))
-            #q_2 = TransmonPocket(design, 'Q2', options=dict(pos_x='+1.5mm', pos_y='+0.0mm'))
-            #q_3 = TransmonPocket(design, 'Q3', options=dict(pos_x='+0.0mm', pos_y='+1.0mm'))
-            #gui = MetalGUI(design)
-            #gui.autoscale()
-            #gui.rebuild(False)
-            #gui.rebuild(True)
+            design = designs.DesignPlanar()
+            q_1 = TransmonPocket(design, 'Q1', options=dict(pos_x='-1.5mm', pos_y='+0.0mm'))
+            q_2 = TransmonPocket(design, 'Q2', options=dict(pos_x='+1.5mm', pos_y='+0.0mm'))
+            q_3 = TransmonPocket(design, 'Q3', options=dict(pos_x='+0.0mm', pos_y='+1.0mm'))
+            gui = MetalGUI(design)
+            gui.autoscale()
+            gui.rebuild(False)
+            gui.rebuild(True)
         except Exception:
             my_name = inspect.stack()[0][3]
             self.fail(my_name + " threw an exception.  GUI failure")
