cb3e55ef03019e8153fd7e95ebb4a6fd9805bc40
Zlatko Minev <zminev@gmail.com>
2020-06-16 22:02:27 -0400

    QComponent Template update

--- qiskit_metal/components/_template.py
+++ qiskit_metal/components/_template.py
@@ -1,10 +1,11 @@
 from qiskit_metal import Dict, QComponent
 from qiskit_metal import draw, is_true
+from qiskit_metal.components.base import BaseQubit
 
 class MyQComponent(QComponent):
+    """Write documentation here."""
 
-    # EDIT HERE
-    # Define the default tempate options
+    # EDIT HERE -- Define the default tempate options
     default_options = Dict(
         width='500um',
         height='300um',
@@ -18,11 +19,10 @@ class MyQComponent(QComponent):
 
         p = self.parse_options()  # Parse the string options into numbers
 
+        ########################################################################
         # EDIT HERE - Replace the following with your code
-
         # Create some raw geometry -- See autocompletion for the `draw` module
         rect = draw.rectangle(p.width, p.height, p.pos_x, p.pos_y)
         rect = draw.rotate(rect, p.rotation)
-        # Create QGeometry from a polygon
         geom = {'my_polygon': rect}
-        self.add_elements('poly', geom, layer=p.layer, subtract=False)
+        self.add_elements('poly', geom, layer=p.layer, subtract=False) # Create QGeometry from a polygon
