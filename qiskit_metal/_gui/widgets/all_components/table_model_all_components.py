5a8fb20c7bb3e161fe856158b247c00be017629f
Zlatko Minev <zminev@gmail.com>
2020-07-10 23:33:52 -0400

    Add id and fix show header

--- qiskit_metal/_gui/widgets/all_components/table_model_all_components.py
+++ qiskit_metal/_gui/widgets/all_components/table_model_all_components.py
@@ -51,7 +51,7 @@ class QTableModel_AllComponents(QAbstractTableModel):
         self.gui = gui
         self._tableView = tableView  # the view used to preview this model, used to refresh
         self.columns = ['Name', 'QComponent class',
-                        'QComponent module', 'Build status']
+                        'QComponent module', 'Build status', 'id']
         self._row_count = -1
 
         self._create_timer()
@@ -94,6 +94,9 @@ class QTableModel_AllComponents(QAbstractTableModel):
             # TODO: This seems overkill to just change the total number of rows?
             self.modelReset.emit()
 
+            # for some reason the horozontal header is hidden even if i call this in init
+            self._tableView.horizontalHeader().show()
+
             self._row_count = new_count
             self.update_view()
 
@@ -116,12 +119,13 @@ class QTableModel_AllComponents(QAbstractTableModel):
     def columnCount(self, parent: QModelIndex = None):
         return len(self.columns)
 
-    def headerData(self, section, orientation, role=Qt.DisplayRole):
+    def headerData(self, section, orientation: Qt.Orientation, role=Qt.DisplayRole):
         """ Set the headers to be displayed. """
 
         if role == Qt.DisplayRole:
             if orientation == Qt.Horizontal:
                 if section < len(self.columns):
+                    # print(self.columns[section])
                     return self.columns[section]
 
         elif role == Qt.FontRole:
@@ -158,13 +162,15 @@ class QTableModel_AllComponents(QAbstractTableModel):
         if role == Qt.DisplayRole:
 
             if index.column() == 0:
-                return component_name
+                return str(component_name)
             elif index.column() == 1:
-                return self.design.components[component_name].__class__.__name__
+                return str(self.design.components[component_name].__class__.__name__)
             elif index.column() == 2:
-                return self.design.components[component_name].__class__.__module__
+                return str(self.design.components[component_name].__class__.__module__)
             elif index.column() == 3:
-                return self.design.components[component_name].status
+                return str(self.design.components[component_name].status)
+            elif index.column() == 4:
+                return str(self.design.components[component_name].id)
 
         # The font used for items rendered with the default delegate. (QFont)
         elif role == Qt.FontRole:
