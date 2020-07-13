0b66cbbd30bd4186204187089f4c22f44f2148ab
Thomas G McConkey <Thomas.G.McConkey@ibm.com>
2020-07-13 16:06:37 -0400

    Added simple bump_pad
    -fixed up a bit of docs
    -added a basic single plane bump pad

--- qiskit_metal/components/connectors/open_to_ground.py
+++ qiskit_metal/components/connectors/open_to_ground.py
@@ -21,7 +21,7 @@ from qiskit_metal.components.base import QComponent
 
 
 class OpenToGround(QComponent):
-    """A basic short to ground termination. Functions as a pin for auto CPW drawing.
+    """A basic open to ground termination. Functions as a pin for auto CPW drawing.
 
     Inherits `QComponent` class
 
@@ -29,13 +29,14 @@ class OpenToGround(QComponent):
         Convention: Values (unless noted) are strings with units included,
         (e.g., '30um')
 
-        width       - the width of the 'cpw' terminating to ground (this is merely
-                    for the purpose of generating a value to pass to the pin)
-        pos_x/_y    - the x/y position of the ground termination.
-        rotation    - the direction of the termination. 0 degrees is +x, following a
-                    counter-clockwise rotation (eg. 90 is +y)
-        chip        - the chip the pin should be on.
-        layer       - layer the pin is on. Does not have any practical impact to the short.
+        width           - the width of the 'cpw' terminating to ground.
+        gap             - the gap of the 'cpw'
+        termination_gap - the length of dielectric from the end of the cpw center trace to the ground.
+        pos_x/_y        - the x/y position of the ground termination.
+        rotation        - the direction of the termination. 0 degrees is +x, following a
+                        counter-clockwise rotation (eg. 90 is +y)
+        chip            - the chip the pin should be on.
+        layer           - layer the pin is on. Does not have any practical impact to the short.
     """
 
     default_options = Dict(
