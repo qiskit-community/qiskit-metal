b397dcd4d34aaa191d0474cb21e65c067bb42ca7
Priti Ashvin Shah <priti.ashvin.shah@ibm.com>
2020-07-21 09:15:31 -0400

    Update imports.

--- qiskit_metal/renderers/renderer_gds/gds_renderer.py
+++ qiskit_metal/renderers/renderer_gds/gds_renderer.py
@@ -14,10 +14,6 @@ from ...toolbox_python.utility_functions import log_error_easy
 
 from qiskit_metal.renderers.renderer_base import QRenderer
 
-from PyQt5.QtWidgets import QInputDialog, QLineEdit, QTableView, QMenu, QMessageBox, QAbstractItemView
-from PyQt5.QtCore import QPoint, QModelIndex
-from PyQt5.QtGui import QContextMenuEvent
-from PyQt5 import QtWidgets, QtCore, QtGui
 from qiskit_metal.components.qubits.transmon_pocket import TransmonPocket
 from qiskit_metal import MetalGUI, Dict, Headings
 from qiskit_metal import components as qlibrary
