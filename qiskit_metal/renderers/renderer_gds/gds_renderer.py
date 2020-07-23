8f137610873831e8663e85a81bdd956b3fc7aec1
JEREMY DRYSDALE <jdrysda@us.ibm.com>
2020-07-23 06:08:01 -0400

    fixed merge conflict in gds_renderer.py

--- qiskit_metal/renderers/renderer_gds/gds_renderer.py
+++ qiskit_metal/renderers/renderer_gds/gds_renderer.py
@@ -37,14 +37,9 @@ class GDSRender(QRenderer):
     def __init__(self, design: QDesign, initiate=True, bounding_box_scale: float = 1.2):
         """
         Args:
-<<<<<<< HEAD
-            design (QDesign): Use QGeometry within QDesign  to obtain elements for GDS file.
-            initiate (bool, optional): True to initiate the renderer. Defaults to True.
-            bounding_box_scale (float, optional): Scale box of components to render. Should be greater than 1.0.
-=======
             design (QDesign): Use QGeometry within QDesign  to obtain elements for GDS file. 
             initiate (bool): True to initiate the renderer. Defaults to True.
->>>>>>> v0.2-dev
+            bounding_box_scale (float): Scale box of components to render. Should be greater than 1.0.
         """
         super().__init__(design=design, initiate=initiate)
         self.gds_unit = self.design.get_units()
