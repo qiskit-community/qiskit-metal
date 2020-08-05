b61efca07295bdff655072309c1c0e2c2c84c9d2
JEREMY DRYSDALE <jdrysda@us.ibm.com>
2020-08-05 08:05:33 -0400

    hardening component unit tests

--- qiskit_metal/tests/__init__.py
+++ qiskit_metal/tests/__init__.py
@@ -32,7 +32,8 @@ Tests
     :toctree: ../stubs/
 
     TestAnalyses
-    TestComponents
+    TestComponentInstantiation
+    TestComponentFunctionality
     TestDefautOptions
     TestDesign
     TestDraw
@@ -57,7 +58,8 @@ from .. import config
 if config.is_building_docs():
     from .assertions import AssertionsMixin
     from .test_analyses import TestAnalyses
-    from .test_components import TestComponents
+    from .test_components_1_instantiate import TestComponentInstantiation
+    from .test_components_2_functionality import TestComponentFunctionality
     from .test_default_options import TestDefautOptions
     from .test_designs import TestDesign
     from .test_draw import TestDraw
