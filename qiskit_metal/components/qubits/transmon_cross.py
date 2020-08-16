c3a8db72f6aa361ac948f940e63cc2d0e0e391e8
Helena Zhang <Helena.Zhang@ibm.com>
2020-08-16 02:32:02 -0400

    removed unnecessary prefix

--- qiskit_metal/components/qubits/transmon_cross.py
+++ qiskit_metal/components/qubits/transmon_cross.py
@@ -75,9 +75,6 @@ class TransmonCross(BaseQubit):  # pylint: disable=invalid-name
     .. image::
         Component_Qubit_Transmon_Crossmon.png
     """
-    component_metadata = Dict(
-        component_type='Q'
-        )
 
     #_img = 'Metal_Crossmon.png'
 
