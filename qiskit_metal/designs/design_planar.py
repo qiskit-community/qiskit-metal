2adc0ebff5aef9d04120da8c77601c99f8c251f7
Priti Shah <Priti.Ashvin.Shah@ibm.com>
2020-08-18 13:38:01 -0400

    Use parse_value.

--- qiskit_metal/designs/design_planar.py
+++ qiskit_metal/designs/design_planar.py
@@ -54,14 +54,14 @@ class DesignPlanar(QDesign):
 
         # GDSPY is using numbers based on 1 meter unit.
         # When the gds file is exported, data is converted to "user-selected" units.
-        # centered at (0,0) and 5 mm by 5 mm size.
+        # centered at (0,0) and 9 by 6 size.
 
         NOTE: self._chips dict comes from QDesign base class.
         """
         self._chips['main'] = {}
 
         self._chips['main']['size'] = {
-            'center_x': 0.0, 'center_y': 0.0, 'size_x': 9, 'size_y': 6}
+            'center_x': '0.0', 'center_y': '0.0', 'size_x': '9', 'size_y': '6'}
 
     def get_x_y_for_chip(self, chip_name: str) -> Tuple[tuple, int]:
         """If the chip_name is in self.chips, along with entry for size information
@@ -79,23 +79,15 @@ class DesignPlanar(QDesign):
 
         if chip_name in self._chips:
             if 'size' in self._chips[chip_name]:
-                size = self.chips[chip_name]['size']
+                size = self.parse_value(self.chips[chip_name]['size'])
                 if 'center_x' in size and 'center_y' in size and 'size_x' in size and 'size_y' in size:
-                    if (isinstance(size['center_x'], int) or isinstance(size['center_x'], float)) and \
-                       (isinstance(size['center_y'], int) or isinstance(size['center_y'], float)) and \
-                       (isinstance(size['size_x'], int) or isinstance(size['size_x'], float)) and \
-                       (isinstance(size['size_y'], int) or isinstance(size['size_y'], float)):
-                        x_y_location = (
-                            size['center_x'] - (size['size_x'] / 2.0),
-                            size['center_y'] - (size['size_y'] / 2.0),
-                            size['center_x'] + (size['size_x'] / 2.0),
-                            size['center_y'] + (size['size_y'] / 2.0)
-                        )
-                        return x_y_location, 0
-                    else:
-                        self.logger.warning(
-                            f'Size information within self.chips[{chip_name}]["size"] is NOT an int or float.')
-                        return x_y_location, 2
+                    x_y_location = (
+                        size['center_x'] - (size['size_x'] / 2.0),
+                        size['center_y'] - (size['size_y'] / 2.0),
+                        size['center_x'] + (size['size_x'] / 2.0),
+                        size['center_y'] + (size['size_y'] / 2.0)
+                    )
+                    return x_y_location, 0
                 else:
                     self.logger.warning(
                         f'center_x or center_y or size_x or size_y NOT in self._chips[{chip_name}]["size"]')
@@ -104,7 +96,6 @@ class DesignPlanar(QDesign):
                 self.logger.warning(
                     f'Information for size in NOT in self._chips[{chip_name}] dict. Return "None" in tuple.')
                 return x_y_location, 2
-
         else:
             self.logger.warning(
                 f'Chip name "{chip_name}" is not in self._chips dict. Return "None" in tuple.')
