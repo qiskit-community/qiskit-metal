b65e139d34d1cd101a995cc9e5dabc9ab7e02b37
Helena Zhang <Helena.Zhang@ibm.com>
2020-08-20 18:26:00 -0400

    added multizoom

--- qiskit_metal/renderers/renderer_mpl/mpl_canvas.py
+++ qiskit_metal/renderers/renderer_mpl/mpl_canvas.py
@@ -601,19 +601,30 @@ class PlotCanvas(FigureCanvas):
             # ax.redraw_in_frame()
             self.refresh()
 
-    # TODO: move to base class
-    def zoom_on_component(self, name: str, zoom: float = 1.2):
+    def find_component_bounds(self, components: List[str], zoom: float = 1.2):
         """Zoom in on a component
 
         Arguments:
             name (str): component name
             zoom (float): fraction to expand the bounding vbox by
         """
-        component = self.design.components[name]
-        bounds = component.qgeometry_bounds()  # return (minx, miny, maxx, maxy)
-        bbox = Bbox.from_extents(bounds)
-        bounds = bbox.expanded(zoom, zoom).extents
-        self.zoom_to_rectangle(bounds)
+        if len(components) == 0:
+            assert "At least one component must be provided."
+
+        # initialize bounds
+        bounds = [float("inf"), float("inf"), float("-inf"), float("-inf")]
+        for name in components:
+            component = self.design.components[name]
+            # return (minx, miny, maxx, maxy)
+            newbounds = component.qgeometry_bounds()
+            bbox = Bbox.from_extents(newbounds)
+            newbounds = bbox.expanded(zoom, zoom).extents
+            # re-calculate total bounds by adding current component
+            bounds = [min(newbounds[0], bounds[0]),
+                      min(newbounds[1], bounds[1]),
+                      max(newbounds[2], bounds[2]),
+                      max(newbounds[3], bounds[3])]
+        return bounds
 
     def set_component(self, name: str):
         """
