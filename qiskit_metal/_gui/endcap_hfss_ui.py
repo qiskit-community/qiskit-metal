f2b7484aac812a429c64c2a293e87ab4691542ae
Dennis Wang <Dennis.Wang@ibm.com>
2021-01-05 21:56:17 -0800

    Expand HFSS endcap window

--- qiskit_metal/_gui/endcap_hfss_ui.py
+++ qiskit_metal/_gui/endcap_hfss_ui.py
@@ -3,7 +3,7 @@
 # Form implementation generated from reading ui file 'endcap_hfss_ui.ui',
 # licensing of 'endcap_hfss_ui.ui' applies.
 #
-# Created: Wed Dec 16 22:00:01 2020
+# Created: Tue Jan  5 21:55:25 2021
 #      by: pyside2-uic  running on PySide2 5.13.2
 #
 # WARNING! All changes made in this file will be lost!
@@ -13,14 +13,14 @@ from PySide2 import QtCore, QtGui, QtWidgets
 class Ui_mainWindow(object):
     def setupUi(self, mainWindow):
         mainWindow.setObjectName("mainWindow")
-        mainWindow.resize(322, 530)
+        mainWindow.resize(400, 530)
         self.centralwidget = QtWidgets.QWidget(mainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setGeometry(QtCore.QRect(10, 10, 271, 20))
         self.label.setObjectName("label")
         self.verticalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
-        self.verticalLayoutWidget.setGeometry(QtCore.QRect(100, 450, 131, 32))
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(130, 450, 131, 32))
         self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
         self.verticalLayout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)
@@ -29,14 +29,14 @@ class Ui_mainWindow(object):
         self.renderButton.setObjectName("renderButton")
         self.verticalLayout.addWidget(self.renderButton)
         self.tableWidget = QtWidgets.QTableWidget(self.centralwidget)
-        self.tableWidget.setGeometry(QtCore.QRect(10, 30, 301, 421))
+        self.tableWidget.setGeometry(QtCore.QRect(10, 30, 381, 421))
         self.tableWidget.setSizeAdjustPolicy(QtWidgets.QAbstractScrollArea.AdjustIgnored)
         self.tableWidget.setObjectName("tableWidget")
         self.tableWidget.setColumnCount(0)
         self.tableWidget.setRowCount(0)
         mainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar()
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 322, 22))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 400, 22))
         self.menubar.setObjectName("menubar")
         mainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(mainWindow)
