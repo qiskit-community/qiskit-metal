d9426dcc0ba6049a6c30f429cb7d6b962def5e52
Priti A Shah <Priti.Ashvin.Shah@ibm.com>
2021-03-04 12:42:51 -0500

    Remove bad comments.

--- qiskit_metal/renderers/renderer_ansys/ansys_renderer.py
+++ qiskit_metal/renderers/renderer_ansys/ansys_renderer.py
@@ -464,23 +464,7 @@ class QAnsysRenderer(QRenderer):
                 "PlotGeomInfo:="     , PlotGeomInfo,
             ]
         #yapf: enable
-
         return oFieldsReport.CreateFieldPlot(args_list, "Field")
-
-        #  return oFieldsReport.CreateFieldPlot(
-        #     [
-        #         "NAME:Mag_E1"        ,
-        #         "SolutionName:="     ,  f"{setup.name} : LastAdaptive",  # name of the setup
-        #         "UserSpecifyName:="  , 0,
-        #         "UserSpecifyFolder:=", 0,
-        #         "QuantityName:="     , "Mag_E",
-        #         "PlotFolder:="       , "E Field",
-        #         "StreamlinePlot:="   , False,
-        #         "AdjacentSidePlot:=" , False,
-        #         "FullModelPlot:="    , False,
-        #         "IntrinsicVar:="     , "Phase=\'0deg\'",
-        #         "PlotGeomInfo:="     , [1, "Surface", "FacesList", 1, str(object_id)],
-        #     ],  "Field")
 
     def plot_ansys_delete(self, names: list):
         """
