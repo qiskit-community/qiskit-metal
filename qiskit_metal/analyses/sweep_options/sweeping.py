1b2b559917b81daf3b12ff6811b68bd603f44aac
Priti A Shah <Priti.Ashvin.Shah@ibm.com>
2021-03-12 14:41:55 -0500

    Change names from scan to sweep.

--- qiskit_metal/analyses/sweep_options/sweeping.py
+++ qiskit_metal/analyses/sweep_options/sweeping.py
@@ -15,13 +15,13 @@
 from typing import List, Tuple, Union, Any, Iterable
 
 
-class Scanning():
+class Sweeping():
     """
         Need access to renderers which are registered in QDesign.
     """
 
     def __init__(self, design: 'QDesign'):
-        """Give QDesign to this class so Scanning can access the registered QRenderers.
+        """Give QDesign to this class so Sweeping can access the registered QRenderers.
 
         Args:
             design (QDesign): Used to access the QRenderers. 
@@ -42,54 +42,54 @@ class Scanning():
         value = a_dict[search]
         return value
 
-    def scan_one_option_get_capacitance_matrix(
+    def sweep_one_option_get_capacitance_matrix(
             self,
             qcomp_name: str,
             option_name: str,
-            option_scan: list,
+            option_sweep: list,
             qcomp_render: list,
             endcaps_render: list,
             leave_last_design: bool = True) -> Tuple[dict, int]:
         """Ansys must be open with inserted project "Q3D Extractor Design." 
 
         Args:
-            qcomp_name (str): A component that contains the option to be scanned.
-            option_name (str): The option within qcomp_name to scan.
-            option_scan (list): Each entry in the list is a value for option_name.
+            qcomp_name (str): A component that contains the option to be swept.
+            option_name (str): The option within qcomp_name to sweep.
+            option_sweep (list): Each entry in the list is a value for option_name.
             qcomp_render (list): The component to render to Q3D. 
             endcaps_render (list): Identify which kind of pins. Follow the details from renderer QQ3DRenderer.render_design.
-            leave_last_design (bool) : In Q3d, after the last scan, should the design be cleared?
+            leave_last_design (bool) : In Q3d, after the last sweep, should the design be cleared?
 
         Returns:
-            dict: The key is each value of option_scan, the value is the capacitance matrix for each scan.
-            int: Observation of searching for data from agrguments.
+            dict: The key is each value of option_sweep, the value is the capacitance matrix for each sweep.
+            int: Observation of searching for data from arguments.
 
             * 0 Have list of capacitance matrix.
             * 1 qcomp_name not registered in design.
             * 2 option_name is empty.
             * 3 option_name is not found as key in dict.
             * 4 last key in option_name is not in dict.
-            * 5 option_scan is empty, need at least one entry.
+            * 5 option_sweep is empty, need at least one entry.
             * 6 project not in app
             * 7 design not in app
 
            
         """
-        #Dict of all scanned information.
-        all_scan = dict()
+        #Dict of all swept information.
+        all_sweep = dict()
 
-        if len(option_scan) == 0:
-            return all_scan, 5
+        if len(option_sweep) == 0:
+            return all_sweep, 5
 
         if option_name:
             option_path = option_name.split('.')
         else:
-            return all_scan, 2
+            return all_sweep, 2
 
         if qcomp_name in self.design.components.keys():
             qcomp_options = self.design.components[qcomp_name].options
         else:
-            return all_scan, 1
+            return all_sweep, 1
 
         a_value = qcomp_options
 
@@ -99,7 +99,7 @@ class Scanning():
                 a_value = self.option_value(a_value, name)
             else:
                 self.design.logger.warning(f'Key="{name}" is not in dict.')
-                return all_scan, 3
+                return all_sweep, 3
 
         a_q3d = self.design.renderers.q3d
         a_q3d.connect_ansys()
@@ -110,16 +110,16 @@ class Scanning():
 
         a_q3d.add_q3d_setup()  # Add a solution setup.
 
-        len_scan = len(option_scan) - 1
+        len_sweep = len(option_sweep) - 1
 
         # Last item in list.
-        for index, item in enumerate(option_scan):
+        for index, item in enumerate(option_sweep):
             if option_path[-1] in a_value.keys():
                 a_value[option_path[-1]] = item
             else:
                 self.design.logger.warning(
                     f'Key="{option_path[-1]}" is not in dict.')
-                return all_scan, 4
+                return all_sweep, 4
 
             self.design.rebuild()
 
@@ -132,18 +132,18 @@ class Scanning():
             a_q3d.analyze_setup("Setup")  #Analyze said solution setup.
             cap_matrix = a_q3d.get_capacitance_matrix()
 
-            scan_values = dict()
-            scan_values['option_name'] = option_path[-1]
-            scan_values['capacitance'] = cap_matrix
-            all_scan[item] = scan_values
+            sweep_values = dict()
+            sweep_values['option_name'] = option_path[-1]
+            sweep_values['capacitance'] = cap_matrix
+            all_sweep[item] = sweep_values
 
             #Decide if need to clean the design.
             obj_names = a_q3d.pinfo.get_all_object_names()
             if obj_names:
-                if index == len_scan and not leave_last_design:
+                if index == len_sweep and not leave_last_design:
                     a_q3d.clean_active_design()
-                elif index != len_scan:
+                elif index != len_sweep:
                     a_q3d.clean_active_design()
-        return all_scan, 0
+        return all_sweep, 0
 
-    # The methods allow users to scan a variable in a components's options.
+    # The methods allow users to sweep a variable in a components's options.
