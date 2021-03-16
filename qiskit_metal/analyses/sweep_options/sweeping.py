9cdd0e68b8a6504938f1485fecc3295dc1166978
Priti A Shah <Priti.Ashvin.Shah@ibm.com>
2021-03-16 13:45:08 -0400

    Save code before looking for solution type in Ansys.

--- qiskit_metal/analyses/sweep_options/sweeping.py
+++ qiskit_metal/analyses/sweep_options/sweeping.py
@@ -16,8 +16,8 @@ from typing import List, Tuple, Union, Any, Iterable
 
 
 class Sweeping():
-    """
-        Need access to renderers which are registered in QDesign.
+    """  The methods allow users to sweep a variable in a components's options.  
+    Need access to renderers which are registered in QDesign.
     """
 
     def __init__(self, design: 'QDesign'):
@@ -37,10 +37,113 @@ class Sweeping():
             search (str): String to search for
 
         Returns:
-            str: Value from the dictionary of the searched term        
+            str: Value from the dictionary of the searched term
         """
         value = a_dict[search]
         return value
+
+    '''all_sweeps, return_code = sweep.sweep_one_option_get_eigenmode_solution_data(
+    q2.name,
+    'connection_pads.c.pad_width', 
+    ['100um', '110um', '120um'],
+    design_name="GetCapacitance")'''
+
+    def sweep_one_option_get_eigenmode_solution_data(
+            self,
+            qcomp_name: str,
+            option_name: str,
+            option_sweep: list,
+            qcomp_render: list,
+            endcaps_render: list,
+            leave_last_design: bool = True,
+            design_name: str = "Sweep_Capacitance") -> Tuple[dict, int]:
+        """Ansys must be open with inserted project. A design, "HFSS Design" with eigenmode solution-type
+        will be inserted by this method.
+
+        Args:
+            qcomp_name (str): A component that contains the option to be swept.
+            option_name (str): The option within qcomp_name to sweep.
+            option_sweep (list): Each entry in the list is a value for option_name.
+            leave_last_design (bool) : In HFSS, after the last sweep, should the design be cleared?
+            endcaps_render (list): Identify which kind of pins. Follow the details from renderer QAnsysRenderer.render_design.
+            leave_last_design (bool) : In HFSS, after the last sweep, should the design be cleared?
+            design_name (str, optional):  Name of hfss_design to use in project. Defaults to "Sweep_Eigenmode".
+            
+
+        Returns:
+            Tuple[dict, int]: 
+            dict: The key is each value of option_sweep, the value is the solution-data for each sweep.
+            int: Observation of searching for data from arguments.
+
+            * 0 Have list of capacitance matrix.
+            * 1 qcomp_name not registered in design.
+            * 2 option_name is empty.
+            * 3 option_name is not found as key in dict.
+            * 4 last key in option_name is not in dict.
+            * 5 option_sweep is empty, need at least one entry.
+            * 6 project not in app
+            * 7 design not in app
+
+        """
+
+        #Dict of all swept information.
+        all_sweep = dict()
+
+        if len(option_sweep) == 0:
+            return all_sweep, 5
+
+        if option_name:
+            option_path = option_name.split('.')
+        else:
+            return all_sweep, 2
+
+        if qcomp_name in self.design.components.keys():
+            qcomp_options = self.design.components[qcomp_name].options
+        else:
+            return all_sweep, 1
+
+        a_value = qcomp_options
+
+        # All but the last item in list.
+        for name in option_path[:-1]:
+            if name in a_value:
+                a_value = self.option_value(a_value, name)
+            else:
+                self.design.logger.warning(f'Key="{name}" is not in dict.')
+                return all_sweep, 3
+
+        a_hfss = self.design.renderers.hfss
+        a_hfss.connect_ansys()
+        a_hfss.activate_eigenmode_design(design_name)
+
+        obj_names = a_hfss.pinfo.get_all_object_names()
+        if obj_names:
+            a_hfss.clean_active_design()
+
+        a_hfss.activate_eigenmode_setup()  # Add a default solution setup.
+
+        len_sweep = len(option_sweep) - 1
+
+        for index, item in enumerate(option_sweep):
+            if option_path[-1] in a_value.keys():
+                a_value[option_path[-1]] = item
+            else:
+                self.design.logger.warning(
+                    f'Key="{option_path[-1]}" is not in dict.')
+                return all_sweep, 4
+
+            self.design.rebuild()
+
+            a_hfss.render_design(
+                selection=qcomp_render,
+                open_pins=endcaps_render)  #Render the items chosen
+
+            a_hfss.analyze_setup("Setup")  #Analyze said solution setup.
+            setup = a_hfss.pinfo.setup
+            solution_name = a_hfss.pinfo.setup.solution_name
+            a = 5  # for breakpoint
+
+        return all_sweep, 0
 
     def sweep_one_option_get_capacitance_matrix(
             self,
@@ -51,14 +154,15 @@ class Sweeping():
             endcaps_render: list,
             leave_last_design: bool = True,
             design_name: str = "Sweep_Capacitance") -> Tuple[dict, int]:
-        """Ansys must be open with inserted project "Q3D Extractor Design." 
+        """Ansys must be open with inserted project.  A design, "Q3D Extractor Design", 
+        will be inserted by this method. 
 
         Args:
             qcomp_name (str): A component that contains the option to be swept.
             option_name (str): The option within qcomp_name to sweep.
             option_sweep (list): Each entry in the list is a value for option_name.
             qcomp_render (list): The component to render to Q3D. 
-            endcaps_render (list): Identify which kind of pins. Follow the details from renderer QQ3DRenderer.render_design.
+            endcaps_render (list): Identify which kind of pins. Follow the details from renderer QAnsysRenderer.render_design.
             leave_last_design (bool) : In Q3d, after the last sweep, should the design be cleared?
             design_name(str): Name of q3d_design to use in project.
 
@@ -147,5 +251,3 @@ class Sweeping():
                 elif index != len_sweep:
                     a_q3d.clean_active_design()
         return all_sweep, 0
-
-    # The methods allow users to sweep a variable in a components's options.
