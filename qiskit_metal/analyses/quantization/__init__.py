8b8e96a6b374cab5e9faf285d1a24cb77b712a81
marcolincs <marco.facchini2@ibm.com>
2021-08-13 10:31:03 -0400

    trigger

--- qiskit_metal/analyses/quantization/__init__.py
+++ qiskit_metal/analyses/quantization/__init__.py
@@ -11,3 +11,4 @@
 # Any modifications or derivative works of this code must retain this
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
+
