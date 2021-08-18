eb92de2c714f79409923b102c6c07b4d6ba4ada8
Connor Bowman <connorbowman@rum.watson.ibm.com>
2021-08-18 13:50:15 -0400

    small tweaks to tutorials

--- qiskit_metal/analyses/quantization/sim_scattering_impedance.py
+++ qiskit_metal/analyses/quantization/sim_scattering_impedance.py
@@ -280,7 +280,7 @@ class ScatteringImpedanceSim(QAnalysisRenderer):
                   default_graph_data: dict = None):
         if default_graph_data is None:
             default_graph_data = {}
-            # Add the plots here desired to be plotted in the Premade section of the dashboard
-            # See sim_eigenmode dashboard method for reference.
+            # Add the plots here desired to be plotted in the Premade graphs section of the dashboard
+            # See sim_eigenmode's dashboard method for reference.
         return super().dashboard(mode=mode,
                                  default_graph_data=default_graph_data)
