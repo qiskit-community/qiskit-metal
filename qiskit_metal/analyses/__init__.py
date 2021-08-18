ecb6550ee8b4f5b6401f3fb70725ec7a7a38a10d
Priti Ashvin Shah <74020801+priti-ashvin-shah-ibm@users.noreply.github.com>
2021-08-18 12:44:30 -0400

    653 sweep inside q anslysis (#673)
    * Determine flow of connecting to simulation software.
    
    * Get one loop to work, but still have to get setup to work and get the data without knowing which kind of simulation it is.
    
    * Change the flow to run_lom.
    
    * Keep setup name in Ansys to just one. Remove setup activity sweeper.  It will be handled outside of sweeper.  Update sweeping to use new interface for sweeping.
    
    * To avoid cyclical import, don't use QAnalysis in the import here.
    
    * Add an argument for hfss.
    
    * All of argc should not be passed to run(), so manipulate the args and kwarg.
    
    * Show user how to close the simulation software.
    
    * Use updated simulation and analysis interface.
    
    * Update logic so that user doesn't need to re-enter render_design.
    
    * Change user experience so that it as all or nothing for using the previous_run.
    
    * Rename the directory.  Also remove test for Sweeping class since user will not use that directly.  Now user will use run_sweep within QAnalysis.  QAnalysis will make an instance of Sweeper class now.
    
    * Updated 4.21, 4.22, 4.23 notebooks.
    
    Co-authored-by: Thomas G McConkey <Thomas.G.McConkey@ibm.com>

--- qiskit_metal/analyses/__init__.py
+++ qiskit_metal/analyses/__init__.py
@@ -58,7 +58,7 @@ Sweeping Options
 .. autosummary::
     :toctree:
 
-    Sweeping
+    Sweeper
 
 Quantization
 ------------
@@ -98,4 +98,4 @@ from .hamiltonian.transmon_charge_basis import Hcpb
 from .hamiltonian import HO_wavefunctions
 from .hamiltonian import transmon_analytics
 from .hamiltonian.transmon_CPB_analytic import Hcpb_analytic
-from .sweep_options.sweeping import Sweeping
+from .sweep_and_optimize.sweeper import Sweeper
