74e80bb256022a65bbc2556d305dbba5f730508d
Sagarika Mukesh <48924652+smukesh25@users.noreply.github.com>
2021-09-14 12:15:34 -0400

    Lint based updates

--- qiskit_metal/qlibrary/qubits/star_qubit.py
+++ qiskit_metal/qlibrary/qubits/star_qubit.py
@@ -16,7 +16,6 @@
 from qiskit_metal import draw, Dict  # , QComponent
 from qiskit_metal.qlibrary.core import QComponent
 from shapely.geometry import CAP_STYLE
-import numpy as np
 
 
 class StarQubit(QComponent):
@@ -42,8 +41,9 @@ class StarQubit(QComponent):
         gap_couplers='25um', # Gap between the star and the coupling resonator
         gap_readout='10um', # Gap between the star and the readout resonator
         connector_length='75um', # Length of the rectangular part of the connector
-        trap_offset='20um', #The offset between coordinates of the trpezoid to define the side wall angle
-        junc_h = '30um', # junction height  
+        trap_offset='20um', #The offset between coordinates of the trpezoid to define
+        #the side wall angle
+        junc_h = '30um', # junction height
         rotation1 = '0.0', # rotation for one of the coupling resonators '36.0', '0.0',
         rotation2 = '72.0', # rotation for the readout resonator '108.0','72.0',
         rotation3 = '144.0', # rotation for one of the coupling resonators '180.0','144.0',
@@ -73,32 +73,44 @@ class StarQubit(QComponent):
         p = self.p  # p for parsed parameters. Access to the parsed options.
 
         # Extracting coordinated from the user input values
-        y1 = -p.center_radius # y-coordinate of the trapezoid to subtract from circle
-        y2 = -p.radius # y-coordinate of the trapezoid to subtract from circle
-        x1 = -p.center_radius/2 # x-coordinate of the trapezoid to subtract from circle
-        x2 = p.center_radius/2 # x-coordinate of the trapezoid to subtract from circle
-        x3 = p.center_radius + p.trap_offset # x-coordinate of the trapezoid to subtract from circle
-        x4 = -(p.center_radius + p.trap_offset) # x-coordinate of the trapezoid to subtract from circle
-        a1 = -(p.center_radius+p.gap_couplers) # y-coordinate of the polygon defining the coupling resonator
-        a2 = -p.radius # y-coordinate of the polygon defining the coupling resonator
-        a3 = p.radius # y-coordinate of the polygon defining the coupling resonator
-        b1 = -(p.center_radius/4) # x-coordinate of the polygon defining the coupling resonator
-        b2 = (p.center_radius/4) # x-coordinate of the polygon defining the coupling resonator
-        b3 = (p.center_radius-(p.gap_couplers)) # x-coordinate of the polygon defining the coupling resonator
-        b4 = -(p.center_radius-(p.gap_couplers)) # x-coordinate of the polygon defining the coupling resonator
-        b5 = -p.radius # x-coordinate of the polygon defining the coupling resonator
-        b6 = p.radius # x-coordinate of the polygon defining the coupling resonator   
-        c1 = -(p.center_radius+p.gap_readout) # y-coordinate of the polygon defining the readout resonator
-        c2 = -p.radius # y-coordinate of the polygon defining the readout resonator
-        c3 = p.radius # y-coordinate of the polygon defining the readout resonator
-        d1 = -(p.center_radius/2 - p.gap_readout) # x-coordinate of the polygon defining the readout resonator
-        d2 =  p.center_radius/2- p.gap_readout # x-coordinate of the polygon defining the readout resonator
-        d3 = p.center_radius # x-coordinate of the polygon defining the readout resonator
-        d4 = -p.center_radius # x-coordinate of the polygon defining the readout resonator
-        d5 = -p.radius # x-coordinate of the polygon defining the readout resonator
-        d6 = p.radius # x-coordinate of the polygon defining the readout resonator
+        coord_y1 = -p.center_radius # y-coordinate of the trapezoid to subtract from circle
+        coord_y2 = -p.radius # y-coordinate of the trapezoid to subtract from circle
+        coord_x1 = -p.center_radius/2 # x-coordinate of the trapezoid to subtract from circle
+        coord_x2 = p.center_radius/2 # x-coordinate of the trapezoid to subtract from circle
+        coord_x3 = p.center_radius + p.trap_offset # x-coordinate of the trapezoid
+        #to subtract from circle
+        coord_x4 = -(p.center_radius + p.trap_offset) # x-coordinate of the
+        #trapezoid to subtract from circle
+        coord_a1 = -(p.center_radius+p.gap_couplers) # y-coordinate of
+        #the polygon defining the coupling resonator
+        coord_a2 = -p.radius # y-coordinate of the polygon defining the coupling resonator
+        coord_a3 = p.radius # y-coordinate of the polygon defining the coupling resonator
+        coord_b1 = -(p.center_radius/4) # x-coordinate of the polygon defining the
+        #coupling resonator
+        coord_b2 = (p.center_radius/4) # x-coordinate of the polygon defining the
+        #coupling resonator
+        coord_b3 = (p.center_radius-(p.gap_couplers)) # x-coordinate of the polygon
+        #defining the coupling resonator
+        coord_b4 = -(p.center_radius-(p.gap_couplers)) # x-coordinate of the polygon
+        #defining the coupling resonator
+        coord_b5 = -p.radius # x-coordinate of the polygon defining the coupling resonator
+        coord_b6 = p.radius # x-coordinate of the polygon defining the coupling resonator
+        coord_c1 = -(p.center_radius+p.gap_readout) # y-coordinate of the polygon
+        #defining the readout resonator
+        coord_c2 = -p.radius # y-coordinate of the polygon defining the readout resonator
+        coord_c3 = p.radius # y-coordinate of the polygon defining the readout resonator
+        coord_d1 = -(p.center_radius/2 - p.gap_readout) # x-coordinate of the polygon
+        #defining the readout resonator
+        coord_d2 =  p.center_radius/2- p.gap_readout # x-coordinate of the polygon
+        #defining the readout resonator
+        coord_d3 = p.center_radius # x-coordinate of the polygon defining the
+        #readout resonator
+        coord_d4 = -p.center_radius # x-coordinate of the polygon defining the
+        #readout resonator
+        coord_d5 = -p.radius # x-coordinate of the polygon defining the readout resonator
+        coord_d6 = p.radius # x-coordinate of the polygon defining the readout resonator
         pocket_w= p.connector_length  # connector pocket width
-        pocket_h=p.connector_length*2  # connector pocket height    
+        pocket_h=p.connector_length*2  # connector pocket height
         pocket_w1=p.gap_couplers  # Connector to the JJ
         pocket_h1=p.center_radius # Connector to the JJ
 
@@ -116,7 +128,9 @@ class StarQubit(QComponent):
         )
         #join_style = getattr(JOIN_STYLE, p.join_style)
         # create the trapezoids
-        coords1 = [((x1 + p.pos_x),(y1 + p.pos_y)),((x2 + p.pos_x),(y1 + p.pos_y)),((x3 + p.pos_x),(y2 + p.pos_y)),((x4 + p.pos_x),(y2 + p.pos_y))]
+        coords1 = [((coord_x1 + p.pos_x),(coord_y1 + p.pos_y)),((coord_x2 + p.pos_x),\
+        (coord_y1 + p.pos_y)),((coord_x3 + p.pos_x),(coord_y2 + p.pos_y)),\
+        ((coord_x4 + p.pos_x),(coord_y2 + p.pos_y))]
         trap_0 = draw.Polygon(coords1)
         trap_1 = draw.rotate(trap_0, p.rotation1, origin=(p.pos_x, p.pos_y))
         trap_2 = draw.rotate(trap_0, p.rotation2, origin=(p.pos_x, p.pos_y))
@@ -125,26 +139,34 @@ class StarQubit(QComponent):
         trap_5 = draw.rotate(trap_0, p.rotation5, origin=(p.pos_x, p.pos_y))
 
         # Define the connectors
-        coords_coupling_resonator = [((b1 + p.pos_x),(a1 + p.pos_y)),((b2 + p.pos_x),(a1 + p.pos_y)),((b3 + p.pos_x),(a2 + p.pos_y)),((b6 + p.pos_x),(a2 + p.pos_y)),((b6 + p.pos_x),(a3 + p.pos_y)),((b5 + p.pos_x),(a3 + p.pos_y)),((b5 + p.pos_x),(a2 + p.pos_y)),((b4 + p.pos_x),(a2 + p.pos_y))]
+        coords_coupling_resonator = [((coord_b1 + p.pos_x),(coord_a1 + p.pos_y)),\
+        ((coord_b2 + p.pos_x),(coord_a1 + p.pos_y)),((coord_b3 + p.pos_x),(coord_a2 + p.pos_y)),\
+        ((coord_b6 + p.pos_x),(coord_a2 + p.pos_y)),((coord_b6 + p.pos_x),(coord_a3 + p.pos_y)),\
+        ((coord_b5 + p.pos_x),(coord_a3 + p.pos_y)),((coord_b5 + p.pos_x),(coord_a2 + p.pos_y)),\
+        ((coord_b4 + p.pos_x),(coord_a2 + p.pos_y))]
         trap_z = draw.Polygon(coords_coupling_resonator)
         trap_a = draw.rotate(trap_z, p.rotation1, origin=(p.pos_x, p.pos_y))
         trap_b = draw.rotate(trap_z, p.rotation2, origin=(p.pos_x, p.pos_y))
         trap_d = draw.rotate(trap_z, p.rotation4, origin=(p.pos_x, p.pos_y))
         trap_e = draw.rotate(trap_z, p.rotation5, origin=(p.pos_x, p.pos_y))
-        coords_readout = [((d1 + p.pos_x),(c1 + p.pos_y)),((d2 + p.pos_x),(c1 + p.pos_y)),((d3 + p.pos_x),(c2 + p.pos_y)),((d6 + p.pos_x),(c2 + p.pos_y)),((d6 + p.pos_x),(c3 + p.pos_y)),((d5 + p.pos_x),(c3 + p.pos_y)),((d5 + p.pos_x),(c2 + p.pos_y)),((d4 + p.pos_x),(c2 + p.pos_y))]
+        coords_readout = [((coord_d1 + p.pos_x),(coord_c1 + p.pos_y)),((coord_d2 + p.pos_x),\
+        (coord_c1 + p.pos_y)),((coord_d3 + p.pos_x),(coord_c2 + p.pos_y)),((coord_d6 + p.pos_x),\
+        (coord_c2 + p.pos_y)),((coord_d6 + p.pos_x),(coord_c3 + p.pos_y)),((coord_d5 + p.pos_x),\
+        (coord_c3 + p.pos_y)),((coord_d5 + p.pos_x),(coord_c2 + p.pos_y)),((coord_d4 + p.pos_x),\
+        (coord_c2 + p.pos_y))]
         trap_c = draw.Polygon(coords_readout)
         trap_c = draw.rotate(trap_c, p.rotation3, origin=(p.pos_x, p.pos_y))
         rect1 = draw.rectangle(pocket_w1, pocket_h1)
-        rect1 = draw.translate(rect1, xoff=p.pos_x + b2*1.1, yoff=p.radius)
+        rect1 = draw.translate(rect1, xoff=p.pos_x + coord_b2*1.1, yoff=p.radius)
         rect1 = draw.rotate(rect1, p.rotation1, origin=(p.pos_x, p.pos_y))
         rect2 = draw.rectangle(pocket_w1, pocket_h1)
-        rect2 = draw.translate(rect2, xoff=p.pos_x + b1*1.1, yoff=p.radius)
+        rect2 = draw.translate(rect2, xoff=p.pos_x + coord_b1*1.1, yoff=p.radius)
         rect2 = draw.rotate(rect2, p.rotation1, origin=(p.pos_x, p.pos_y))
 
 
         # Define contacts
         pocket0 = draw.rectangle(pocket_w, pocket_h)
-        pocket0 = draw.translate(pocket0, xoff=p.pos_x, yoff=(y2 + p.pos_y))
+        pocket0 = draw.translate(pocket0, xoff=p.pos_x, yoff=(coord_y2 + p.pos_y))
         pocket1 = draw.rotate(pocket0, p.rotation1, origin=(p.pos_x, p.pos_y))
         pocket2 = draw.rotate(pocket0, p.rotation2, origin=(p.pos_x, p.pos_y))
         pocket3 = draw.rotate(pocket0, p.rotation3, origin=(p.pos_x, p.pos_y))
@@ -152,26 +174,28 @@ class StarQubit(QComponent):
         pocket5 = draw.rotate(pocket0, p.rotation5, origin=(p.pos_x, p.pos_y))
 
         #Connectors for the ground plane
-        pocketZ = draw.rectangle(pocket_w*1.4, pocket_h)
-        pocketZ = draw.translate(pocketZ, xoff=p.pos_x, yoff=(y2 + p.pos_y))
-        pocketA = draw.rotate(pocketZ, p.rotation1, origin=(p.pos_x, p.pos_y))
-        pocketB = draw.rotate(pocketZ, p.rotation2, origin=(p.pos_x, p.pos_y))
-        pocketC = draw.rotate(pocketZ, p.rotation3, origin=(p.pos_x, p.pos_y))
-        pocketD = draw.rotate(pocketZ, p.rotation4, origin=(p.pos_x, p.pos_y))
-        pocketE = draw.rotate(pocketZ, p.rotation5, origin=(p.pos_x, p.pos_y))
+        pocket_z = draw.rectangle(pocket_w*1.4, pocket_h)
+        pocket_z = draw.translate(pocket_z, xoff=p.pos_x, yoff=(coord_y2 + p.pos_y))
+        pocket_a = draw.rotate(pocket_z, p.rotation1, origin=(p.pos_x, p.pos_y))
+        pocket_b = draw.rotate(pocket_z, p.rotation2, origin=(p.pos_x, p.pos_y))
+        pocket_c = draw.rotate(pocket_z, p.rotation3, origin=(p.pos_x, p.pos_y))
+        pocket_d = draw.rotate(pocket_z, p.rotation4, origin=(p.pos_x, p.pos_y))
+        pocket_e = draw.rotate(pocket_z, p.rotation5, origin=(p.pos_x, p.pos_y))
         if (p.number_of_connectors) ==0:
-            circle_outer = draw.union(circle_outer,pocketC)
+            circle_outer = draw.union(circle_outer,pocket_c)
         elif (p.number_of_connectors) ==1:
-            circle_outer = draw.union(circle_outer,pocketA,pocketC)
+            circle_outer = draw.union(circle_outer,pocket_a,pocket_c)
         elif (p.number_of_connectors) ==2:
-            circle_outer = draw.union(circle_outer,pocketA,pocketB,pocketC)
+            circle_outer = draw.union(circle_outer,pocket_a,pocket_b,pocket_c)
         elif (p.number_of_connectors) ==3:
-            circle_outer = draw.union(circle_outer,pocketA,pocketB,pocketC,pocketD)
+            circle_outer = draw.union(circle_outer,pocket_a,pocket_b,pocket_c,pocket_d)
         elif (p.number_of_connectors) ==4:
-            circle_outer = draw.union(circle_outer,pocketA,pocketB,pocketC,pocketD,pocketE)
+            circle_outer = draw.union(circle_outer,pocket_a,pocket_b,pocket_c,
+            pocket_d,pocket_e)
 
         #junction
-        pocket6 = draw.LineString([[p.pos_x+b1-(pocket_h1/2), p.pos_y], [p.pos_x+b2+(pocket_h1/2), p.pos_y]])
+        pocket6 = draw.LineString([[p.pos_x+coord_b1-(pocket_h1/2), p.pos_y],\
+        [p.pos_x+coord_b2+(pocket_h1/2), p.pos_y]])
         pocket6 = draw.translate(pocket6, yoff=(p.pos_y + 1.15*(p.radius)))
         pocket6 = draw.rotate(pocket6, p.rotation1, origin=(p.pos_x, p.pos_y))
 
@@ -225,12 +249,10 @@ class StarQubit(QComponent):
                             helper=p.helper,
                             layer=p.layer,
                             chip=p.chip)
-
             self.add_pin('pin3',
                      pins3.coords,
                      width=0.01,
                      input_as_norm=True)
-        
         elif (p.number_of_connectors) ==1:
             self.add_qgeometry('poly', {'contact1': contact1},
                             subtract=p.subtract,
@@ -252,8 +274,6 @@ class StarQubit(QComponent):
                      pins3.coords,
                      width=0.01,
                      input_as_norm=True)
-
-
         elif (p.number_of_connectors) ==2:
             self.add_qgeometry('poly', {'contact1': contact1},
                             subtract=p.subtract,
@@ -277,18 +297,15 @@ class StarQubit(QComponent):
                      input_as_norm=True)
 
             # Define second pin
-            
             self.add_pin('pin2',
                         pins2.coords,
                         width=0.01,
                         input_as_norm=True)
-
             # Define third pin
             self.add_pin('pin3',
                         pins3.coords,
                         width=0.01,
                         input_as_norm=True)
-
         elif (p.number_of_connectors) ==3:
             self.add_qgeometry('poly', {'contact1': contact1},
                             subtract=p.subtract,
@@ -315,25 +332,21 @@ class StarQubit(QComponent):
                      pins1.coords,
                      width=0.01,
                      input_as_norm=True)
-
             # Define second pin
             self.add_pin('pin2',
                         pins2.coords,
                         width=0.01,
                         input_as_norm=True)
-
             # Define third pin
             self.add_pin('pin3',
                         pins3.coords,
                         width=0.01,
                         input_as_norm=True)
-
             # Define fourth pin
             self.add_pin('pin4',
                         pins4.coords,
                         width=0.01,
                         input_as_norm=True)
-
         elif (p.number_of_connectors) ==4:
             self.add_qgeometry('poly', {'contact1': contact1},
                             subtract=p.subtract,
@@ -359,7 +372,7 @@ class StarQubit(QComponent):
                             subtract=p.subtract,
                             helper=p.helper,
                             layer=p.layer,
-                            chip=p.chip)   
+                            chip=p.chip)
             # Add pin connections
             self.add_pin('pin1',
                      pins1.coords,
@@ -384,13 +397,11 @@ class StarQubit(QComponent):
                         width=0.01,
                         input_as_norm=True)
 
-            # Define fifth pin
-            
+            # Define fifth pin            
             self.add_pin('pin5',
                         pins5.coords,
                         width=0.01,
-                        input_as_norm=True)
-                        
+                        input_as_norm=True)                        
         self.add_qgeometry('junction', {'poly': pocket6},
                            subtract=p.subtract,
                            helper=p.helper,
@@ -402,3 +413,4 @@ class StarQubit(QComponent):
                            helper=p.helper,
                            layer=p.layer,
                            chip=p.chip)
+        
