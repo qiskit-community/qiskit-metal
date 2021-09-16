10fa7abd593cb73c174dedc7d46290f7845ad3a9
Axel Ismirlian <ibm-axel@axels-mbp-2.attlocal.net>
2021-09-16 14:19:21 -0500

    Ran style changes

--- examples/qiskit-cubit.py
+++ examples/qiskit-cubit.py
@@ -1,11 +1,13 @@
 from qiskit_metal import designs, draw, MetalGUI, Dict, open_docs
 
+
 def main():
     design = designs.DesignPlanar()
     design.overwrite_enabled = True
     design.chips.main.size.size_x = '11mm'
     design.chips.main.size.size_y = '9mm'
     gui = MetalGUI(design)
+
 
 if __name__ == "__main__":
     main()
--- qiskit_metal/_gui/main_window_rc_rc.py
+++ qiskit_metal/_gui/main_window_rc_rc.py
@@ -68387,10 +68387,15 @@ qt_resource_struct = b"\
 \x00\x00\x03\x0a\x00\x00\x00\x00\x00\x01\x00\x0b\x9bl\
 "
 
+
 def qInitResources():
-    QtCore.qRegisterResourceData(0x01, qt_resource_struct, qt_resource_name, qt_resource_data)
+    QtCore.qRegisterResourceData(0x01, qt_resource_struct, qt_resource_name,
+                                 qt_resource_data)
+
 
 def qCleanupResources():
-    QtCore.qUnregisterResourceData(0x01, qt_resource_struct, qt_resource_name, qt_resource_data)
+    QtCore.qUnregisterResourceData(0x01, qt_resource_struct, qt_resource_name,
+                                   qt_resource_data)
+
 
 qInitResources()
