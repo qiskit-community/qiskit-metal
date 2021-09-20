e15d7836846c32fd9308766401ffb6adaec10729
Sagarika Mukesh <48924652+smukesh25@users.noreply.github.com>
2021-09-20 11:11:38 -0400

    Update star_qubit.py

--- qiskit_metal/qlibrary/qubits/star_qubit.py
+++ qiskit_metal/qlibrary/qubits/star_qubit.py
@@ -54,12 +54,12 @@ class StarQubit(QComponent):
                            gap_readout='10um',
                            connector_length='75um',
                            trap_offset='20um',
-                           junc_h = '30um',
-                           rotation1 = '0.0',
-                           rotation2 = '72.0',
-                           rotation3 = '144.0',
-                           rotation4 = '216.0',
-                           rotation5 = '288.0',
+                           junc_h='30um',
+                           rotation1='0.0',
+                           rotation2='72.0',
+                           rotation3='144.0',
+                           rotation4='216.0',
+                           rotation5='288.0',
                            pos_x='0um',
                            pos_y='0um',
                            number_of_connectors='1',
@@ -126,32 +126,32 @@ class StarQubit(QComponent):
         # Extracting coordinated from the user input values
         coord_y1 = -p.center_radius
         coord_y2 = -p.radius
-        coord_x1 = -p.center_radius/2
-        coord_x2 = p.center_radius/2
+        coord_x1 = -p.center_radius / 2
+        coord_x2 = p.center_radius / 2
         coord_x3 = p.center_radius + p.trap_offset
         coord_x4 = -(p.center_radius + p.trap_offset)
-        coord_a1 = -(p.center_radius+p.gap_couplers)
+        coord_a1 = -(p.center_radius + p.gap_couplers)
         coord_a2 = -p.radius
         coord_a3 = p.radius
-        coord_b1 = -(p.center_radius/4)
-        coord_b2 = (p.center_radius/4)
-        coord_b3 = (p.center_radius-(p.gap_couplers))
-        coord_b4 = -(p.center_radius-(p.gap_couplers))
+        coord_b1 = -(p.center_radius / 4)
+        coord_b2 = (p.center_radius / 4)
+        coord_b3 = (p.center_radius - (p.gap_couplers))
+        coord_b4 = -(p.center_radius - (p.gap_couplers))
         coord_b5 = -p.radius
         coord_b6 = p.radius
         coord_c1 = -(p.center_radius+p.gap_readout)
         coord_c2 = -p.radius
         coord_c3 = p.radius
-        coord_d1 = -(p.center_radius/2 - p.gap_readout)
-        coord_d2 =  p.center_radius/2- p.gap_readout
+        coord_d1 = -(p.center_radius / 2 - p.gap_readout)
+        coord_d2 =  p.center_radius  /2- p.gap_readout
         coord_d3 = p.center_radius
         coord_d4 = -p.center_radius
         coord_d5 = -p.radius
         coord_d6 = p.radius
-        pocket_w= p.connector_length
-        pocket_h=p.connector_length*2
-        pocket_w1=p.gap_couplers
-        pocket_h1=p.center_radius
+        pocket_w = p.connector_length
+        pocket_h = p.connector_length * 2
+        pocket_w1 = p.gap_couplers
+        pocket_h1 = p.center_radius
 
         # create a circle
         circle = draw.Point(p.pos_x,\
@@ -202,7 +202,6 @@ class StarQubit(QComponent):
                                yoff=p.radius)
         rect2 = draw.rotate(rect2, p.rotation1, origin=(p.pos_x, p.pos_y))
 
-
         # Define contacts
         pocket0 = draw.rectangle(pocket_w, pocket_h)
         pocket0 = draw.translate(pocket0,
@@ -229,13 +228,14 @@ class StarQubit(QComponent):
         elif (p.number_of_connectors) == 1:
             circle_outer = draw.union(circle_outer, pocket_a, pocket_c)
         elif (p.number_of_connectors) == 2:
-            circle_outer = draw.union(circle_outer, pocket_a, pocket_b, pocket_c)
+            circle_outer = draw.union(circle_outer, pocket_a, pocket_b, 
+                                      pocket_c)
         elif (p.number_of_connectors) == 3:
             circle_outer = draw.union(circle_outer, pocket_a, pocket_b,
-            pocket_c, pocket_d)
+                                      pocket_c, pocket_d)
         elif (p.number_of_connectors) == 4:
             circle_outer = draw.union(circle_outer, pocket_a, pocket_b,
-            pocket_c, pocket_d, pocket_e)
+                                      pocket_c, pocket_d, pocket_e)
 
         #junction
         pocket6 = draw.LineString([[p.pos_x+coord_b1-(pocket_h1/2), p.pos_y],\
@@ -412,4 +412,3 @@ class StarQubit(QComponent):
                            helper=p.helper,
                            layer=p.layer,
                            chip=p.chip)
-        
