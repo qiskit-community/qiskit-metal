af89e6f31c65f2d29fd38bf49f8ce0c00fdef2c3
shobhit <shobhitchauhan@northwesternmutual.com>
2021-12-18 23:57:37 -0600

    fixes #669 by updating main_window.py

--- qiskit_metal/_gui/main_window.py
+++ qiskit_metal/_gui/main_window.py
@@ -168,18 +168,20 @@ class QMainWindowExtension(QMainWindowExtensionBase):
                 self.design.save_path = filename
             # save python script to file path
             pyscript = self.design.to_python_script()
-            with open(filename, 'w') as f:
-                f.write(pyscript)
+            #check whether filename is empty or not. Save file only when filename is non-empty. 
+            if len(filename):
+                with open(filename, 'w') as f:
+                    f.write(pyscript)
 
-            #make it clear it's saving
-            saving_dialog = QDialog(self)
-            saving_dialog.setWindowModality(Qt.NonModal)
-            v = QVBoxLayout()
-            saving_dialog.setLayout(v)
-            v.addWidget(QLabel("Saving..."))
-            saving_dialog.open()
-            saving_dialog.show()
-            QTimer.singleShot(200, saving_dialog.close)
+                #make it clear it's saving
+                saving_dialog = QDialog(self)
+                saving_dialog.setWindowModality(Qt.NonModal)
+                v = QVBoxLayout()
+                saving_dialog.setLayout(v)
+                v.addWidget(QLabel("Saving..."))
+                saving_dialog.open()
+                saving_dialog.show()
+                QTimer.singleShot(200, saving_dialog.close)
         else:
             self.logger.info('No design present.')
             QMessageBox.warning(self, 'Warning', 'No design present! Can'
