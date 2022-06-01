f487fba7bd6ef36cb7ad8e9860c3ab92f281cf47
Abeer Vaishnav <abeerv@li-1cbaaccc-32a6-11b2-a85c-a72837ee8989.ibm.com>
2022-05-31 20:06:45 -0400

    Added initial code for rendering

---
+++ qiskit_metal/renderers/renderer_gmsh/gmsh_renderer.py
@@ -0,0 +1,365 @@
+import pandas as pd
+from typing import *
+from collections import defaultdict
+import gmsh
+from sympy import Chi
+
+from ..renderer_base import QRendererAnalysis
+from gmsh_utils import *
+from ...draw.basic import is_rectangle
+
+class QGmshRenderer(QRendererAnalysis):
+    """Abstract base class for all Renderers intended for Analysis.
+    """
+
+    def __init__(self, design: 'QDesign', initiate=False, options: Dict = None):
+        """
+        Args:
+            design (QDesign): The design.
+            initiate (bool): True to initiate the renderer (Default: False).
+            settings (Dict, optional): Used to override default settings. Defaults to None.
+        """
+        super().__init__(design=design,
+                         initiate=initiate,
+                         render_options=options)
+
+        self.render_everything = True
+
+    @property
+    def initialized(self):
+        """Abstract method. Must be implemented by the subclass.
+        Is renderer ready to be used?
+        Implementation must return boolean True if successful. False otherwise.
+        """
+        if self.get_current_model() == self._model_name:
+            return True
+        return False
+
+    @property
+    def current_model(self):
+        return self._model_name
+
+    @property
+    def model(self):
+        return gmsh.model
+
+    def _initiate_renderer(self):
+        gmsh.initialize()
+        return True
+
+    def _close_renderer(self):
+        gmsh.finalize()
+        return True
+
+    def close(self):
+        return self._close_renderer()
+
+    def add_model(self, name:str):
+        self._model_name = name
+        gmsh.model.add(name)
+
+    def set_current_model(self, name:str):
+        gmsh.model.set_current(name)
+
+    def get_current_model(self) -> str:
+        return gmsh.model.get_current()
+
+    def remove_current_model(self):
+        gmsh.model.remove()
+
+    def get_chip_names(self):
+        if self.case == 2:  # One or more components not in QDesign.
+            self.logger.warning("One or more components not found.")
+            return []
+        chip_names = set()
+        if self.case == 1:  # All components rendered.
+            comps = self.design.components
+            for qcomp in comps:
+                if "chip" not in comps[qcomp].options:
+                    self.chip_designation_error()
+                    return []
+                # elif comps[qcomp].options.chip != 'main':
+                #    self.chip_not_main()
+                #    return []
+                chip_names.add(comps[qcomp].options.chip)
+        else:  # Strict subset rendered.
+            icomps = self.design._components
+            for qcomp_id in self.qcomp_ids:
+                if "chip" not in icomps[qcomp_id].options:
+                    self.chip_designation_error()
+                    return []
+                # elif icomps[qcomp_id].options.chip != 'main':
+                #    self.chip_not_main()
+                #    return []
+                chip_names.add(icomps[qcomp_id].options.chip)
+
+        for unique_name in chip_names:
+            if unique_name not in self.design.chips:
+                self.chip_not_in_design_error(unique_name)
+
+        return list(chip_names)
+
+    def chip_designation_error(self):
+        """
+        Warning message that appears when the Ansys renderer fails to locate a component's chip designation.
+        Provides instructions for a temporary workaround until the layer stack is finalized.
+        """
+        self.logger.warning(
+            "This component currently lacks a chip designation. Please add chip='main' to the component's default_options dictionary, restart the kernel, and try again."
+        )
+
+    def chip_not_in_design_error(self, missing_chip: str):
+        """
+        Warning message that appears when the Ansys renderer fails to locate a component's chip designation in DesignPlanar (or any child of QDesign).
+        Provides instructions for a temporary workaround until the layer stack is finalized.
+        """
+        self.logger.warning(
+            f'This component currently lacks a chip designation in DesignPlanar, or any child of QDesign. '
+            f'Please add dict for chip=\'{missing_chip}\' in DesignPlanar, or child of QDesign. Then restart the kernel, and try again.'
+        )
+
+    def render_design(
+        self,
+        selection: Union[list, None] = None,
+        open_pins: Union[list, None] = None,
+        box_plus_buffer: bool = True
+        ):
+
+        self.qcomp_ids, self.case = self.get_unique_component_ids(selection)
+
+        if self.case == 2:
+            self.logger.warning(
+                "Unable to proceed with rendering. Please check selection.")
+            return
+
+        self.chip_subtract_dict = defaultdict(set)
+        self.polys_dict = defaultdict(set)
+        self.paths_dict = defaultdict(set)
+        self.juncs_dict = defaultdict(set)
+        
+        # TODO: sequence of events to perform
+        self.render_tables()
+        # self.add_endcaps(open_pins)
+
+        # self.render_chips(box_plus_buffer=box_plus_buffer)
+        # self.subtract_from_ground()
+        # self.add_mesh()
+
+    def render_tables(self):#, skip_junction: bool = False):
+        """
+        Render components in design grouped by table type (path, poly, or junction).
+        """
+        for table_type in self.design.qgeometry.get_element_types():
+            self.render_components(table_type)
+
+    def render_components(self, table_type: str):
+        """
+        Render components by breaking them down into individual elements.
+
+        Args:
+            table_type (str): Table type (poly, path, or junction).
+        """
+        table = self.design.qgeometry.tables[table_type]
+
+        if self.case == 0:
+            mask = table["component"].isin(self.qcomp_ids)
+            table = table[mask]
+
+        for _, qgeom in table.iterrows():
+            self.render_element(qgeom, table_type)
+
+        if table_type == "path":
+            # TODO: how to do auto wirebonds? (ON HOLD for now)
+            # Make a function to render wire bonds manually?
+            pass
+
+    def render_element(self, qgeom: pd.Series, table_type: str):
+        """Abstract method. Must be implemented by the subclass.
+        Render the specified element
+
+        Args:
+            element (Element): Element to render.
+        """
+        if table_type == "junction":
+            self.render_element_junction(qgeom)
+        elif table_type == "path":
+            self.render_element_path(qgeom)
+        elif table_type == "poly":
+            self.render_element_poly(qgeom)
+        else:
+           self.logger.error(f'RENDERER ERROR: Unkown element type: {table_type}')
+
+    def make_general_surface(self, curves: list[int]) -> int:
+        curve_loop = gmsh.model.occ.addCurveLoop(curves)
+        surface = gmsh.model.occ.addPlaneSurface([curve_loop])
+        return surface
+
+    def parse_units_gmsh(self, _input):
+        _units = {"cm": 10**3, "mm": 1, "um": 10**-3, "nm": 10**-6}
+        if isinstance(_input, (int, float)):
+            return _input
+        elif isinstance(_input, (list, tuple)):
+            output = []
+            for i in _input:
+                output += [self.parse_units_gmsh(i)]
+            return type(_input)(output)
+        elif isinstance(_input, str):
+            # TODO: make this more robust. Might be error prone in the future
+            if 'm' in _input:
+                digits = float(_input[:-2].strip())
+                unit = _input[-2:].strip()
+                try:
+                    value = digits*_units[unit]
+                except KeyError:
+                    value = 0.0
+                    self.logger.error(f"RENDERER ERROR: Unknown unit: {unit}. Use either 'cm', 'mm', 'um', or 'nm'.")
+            else:
+                value = float(_input)
+            return value
+        else:
+            self.logger.error(f"RENDERER ERROR: Expected int, str, list, or tuple. Got: {type(_input)}.")
+
+    def render_element_junction(self, junc: pd.Series):
+        """Render an element junction.
+
+        Args:
+            junc (str): Junction to render.
+        """
+        qc_shapely = junc.geometry
+        qc_width  = self.parse_units_gmsh(junc.width)
+        qc_chip_z = self.parse_units_gmsh(self.design.get_chip_z(junc.chip))
+        vecs = Vec2DArray.make_vec2DArray(self.parse_units_gmsh(list(qc_shapely.coords)))
+        qc_angle = vecs.path_vecs[0].angle()
+
+        # Considering JJ will always be a rectangle
+        # TODO: do we need functionality for arbitrary shape JJ?
+        v1, _ = line_width_offset_pts(vecs.points[0], vecs.path_vecs[0], qc_width, qc_chip_z, ret_pts=False)
+        v3, v4 = line_width_offset_pts(vecs.points[1], vecs.path_vecs[0], qc_width, qc_chip_z, ret_pts=False)
+
+        v1_v3 = v1.dist(v3); v1_v4 = v1.dist(v4)
+        qc_length = v1_v3 if v1_v3 <= v1_v4 else v1_v4
+        rect = gmsh.model.occ.addRectangle(0, 0, qc_chip_z, qc_width, qc_length)
+        gmsh.model.occ.rotate([(2, rect)], 0, 0, 0, 0, 0, 1, qc_angle)
+        gmsh.model.occ.translate([(2, rect)], v1.x, v1.y, 0)
+
+        if junc.chip not in self.juncs_dict:
+            self.juncs_dict[junc.chip] = set()
+
+        self.juncs_dict[junc.chip].add(rect)
+
+    def render_element_path(self, path: pd.Series):
+        """Render an element path.
+
+        Args:
+            path (str): Path to render.
+        """
+        qc_shapely = path.geometry
+        qc_width = path.width
+        qc_fillet = self.parse_units_gmsh(path.fillet) if float(path.fillet) != np.nan else 0.0
+        qc_chip_z = self.parse_units_gmsh(self.design.get_chip_z(path.chip))
+        vecs = Vec2DArray.make_vec2DArray(self.parse_units_gmsh(list(qc_shapely.coords)))
+        lines, arcs = render_path_curves(vecs, qc_chip_z, qc_fillet, qc_width)
+        curves = lines + arcs
+        surface = self.make_general_surface(curves)
+
+        if path.chip not in self.chip_subtract_dict:
+            self.chip_subtract_dict[path.chip] = set()
+
+        if path.chip not in self.paths_dict:
+            self.paths_dict[path.chip] = set()
+
+        if path["subtract"]:
+            self.chip_subtract_dict[path.chip].add(surface)
+        else:
+            self.paths_dict[path.chip].add(surface)
+
+    def make_poly_surface(self, points: list[Vec2D], chip_z: float) -> int:
+        lines = []
+        first_tag = -1
+        prev_tag = -1
+        for i,pt in points[:-1]:
+            p1 = gmsh.model.occ.addPoint(pt.x, pt.y, chip_z) if i == 0 else prev_tag
+            p2 = first_tag if i == (len(points)-2) else \
+                gmsh.model.occ.addPoint(points[i+1].x, points[i+1].y, chip_z)
+            lines += [gmsh.model.occ.addLine(p1, p2)]
+
+            prev_tag = p2
+            if i == 0:
+                first_tag = p1
+
+        return self.make_general_surface(lines)
+
+    def render_element_poly(self, poly: pd.Series):
+        """Render an element poly.
+
+        Args:
+            poly (Poly): Poly to render.
+        """
+        qc_shapely = poly.geometry
+        # FIXME (probably): check if parse units returns proper stuff
+        qc_chip_z = self.parse_units_gmsh(self.design.get_chip_z(poly.chip))
+        points = Vec2DArray.make_vec2DArray(
+            [Vec2D(x, y) for x,y in self.parse_units_gmsh(list(qc_shapely.exterior.coords))])
+
+        if is_rectangle(qc_shapely):
+            x_min, y_min, x_max, y_max = qc_shapely.bounds
+            dx, dy = np.abs(x_max - x_min), np.abs(y_max - y_min)
+            surface = gmsh.model.occ.addRectangle(x_min, y_min, qc_chip_z, dx, dy)
+        else:
+            surface = self.make_poly_surface(points, qc_chip_z)
+
+        if len(qc_shapely.interiors) > 0:
+            int_points = Vec2DArray.make_vec2DArray(
+                [Vec2D(x, y) for x,y in [self.parse_units_gmsh(list(coord)) \
+                    for coord in qc_shapely.interiors]])
+            int_surface = self.make_poly_surface(int_points, qc_chip_z)
+            surface = gmsh.model.occ.cut([surface], [int_surface])
+
+
+        if poly.chip not in self.chip_subtract_dict:
+            self.chip_subtract_dict[poly.chip] = set()
+
+        if poly.chip not in self.polys_dict:
+            self.polys_dict[poly.chip] = set()
+
+        if poly["subtract"]:
+            self.chip_subtract_dict[poly.chip].add(surface)
+        else:
+            self.polys_dict[poly.chip].add(surface)
+
+    def render_chips(self):
+        """Abstract method. Must be implemented by the subclass.
+        Render all chips of the design.
+        Calls render_chip for each chip.
+        """
+        pass
+
+    def render_chip(self, name):
+        """Abstract method. Must be implemented by the subclass.
+        Render the given chip.
+
+        Args:
+            name (str): chip to render
+        """
+        pass
+
+    def render_component(self, component):
+        """Abstract method. Must be implemented by the subclass.
+        Render the specified component.
+
+        Args:
+            component (QComponent): Component to render.
+        """
+        pass
+
+    def save_screenshot(self, path: str = None, show: bool = True):
+        """Save the screenshot.
+
+        Args:
+            path (str, optional): Path to save location.  Defaults to None.
+            show (bool, optional): Whether or not to display the screenshot.  Defaults to True.
+
+        Returns:
+            pathlib.WindowsPath: path to png formatted screenshot. 
+        """
+        pass
---
+++ qiskit_metal/renderers/renderer_gmsh/gmsh_utils.py
@@ -0,0 +1,223 @@
+from dataclasses import dataclass, field
+import numpy as np
+import gmsh
+from qiskit_metal.designs.design_base import QDesign
+
+@dataclass
+class Vec2D:
+    x: float
+    y: float
+    
+    def mag(self):
+        return np.round(np.sqrt(self.x**2 + self.y**2), decimals=9)
+    
+    def normalize(self):
+        mag = self.mag()
+        return Vec2D(x=self.x/mag, y=self.y/mag)
+    
+    def add(self, other):
+        return Vec2D(x=self.x+other.x, y=self.y+other.y)
+    
+    def dot(self, other):
+        return np.round((self.x*other.x) + (self.y*other.y), decimals=9)
+    
+    def cross_prod_sign(self, other):
+        self_norm = self.normalize()
+        other_norm = other.normalize()
+        return (self_norm.x * other_norm.y - self_norm.y * other_norm.x)
+    
+    def sub(self, other):
+        return Vec2D(x=self.x-other.x, y=self.y-other.y)
+    
+    def dist(self, other):
+        return np.round(
+            np.sqrt((self.x - other.x)**2 + (self.y - other.y)**2),
+            decimals=9)
+    
+    def angle(self):
+        if self.x == 0:
+            return np.round(np.arctan(np.sign(self.y) * np.inf), decimals=9)
+        if self.x < 0:
+            return np.round(np.arctan(self.y/self.x) + np.pi, decimals=9)
+        
+        return np.round(np.arctan(self.y/self.x), decimals=9)
+
+@dataclass
+class Vec2DArray:
+    points: list[Vec2D]
+    path_vecs: list[Vec2D] = field(init=False)
+    
+    def __post_init__(self):
+        self.path_vecs = []
+        for i in range(0, len(self.points)-1):
+            v1 = self.points[i]; v2 = self.points[i+1]
+            v12 = v2.sub(v1).normalize()
+            self.path_vecs.append(v12)
+            
+    def append(self, vecs:list[Vec2D]):
+        self.points += vecs
+        
+    def get_angle_between(self, i, j, path_angle:bool=True):
+        if path_angle:
+            v1 = self.path_vecs[i].normalize()
+            v2 = self.path_vecs[j].normalize()
+            return np.round(np.pi - np.arccos(v1.dot(v2)), decimals=9)
+        
+        v1 = self.points[i].normalize()
+        v2 = self.points[j].normalize()
+        return np.round(np.pi - np.arccos(v1.dot(v2)), decimals=9)
+
+
+    @staticmethod
+    def make_vec2DArray(points:list[Vec2D]):
+        vecs = []
+        for xy in points:
+            vecs.append(Vec2D(x=xy[0], y=xy[1]))
+
+        return Vec2DArray(vecs)
+
+
+def vecs_to_gmsh_points(vecs:list[Vec2D], chip_z:float):
+    points = []
+    for v in vecs:
+        p = gmsh.model.occ.addPoint(v.x, v.y, chip_z)
+        points.append(p)
+
+    return points
+
+def line_width_offset_pts(vec:Vec2D, path_vec:Vec2D, width:float, chip_z:float, ret_pts:bool = True):
+    path_angle = path_vec.angle()
+    perp_angle = path_angle + np.pi/2
+    cos_t = np.round(np.cos(perp_angle), decimals=9)
+    sin_t = np.round(np.sin(perp_angle), decimals=9)
+    r = width/2
+
+    v1 = Vec2D(x=np.round(vec.x + r*cos_t, decimals=9),
+               y=np.round(vec.y + r*sin_t, decimals=9))
+    v2 = Vec2D(x=np.round(vec.x - r*cos_t, decimals=9),
+               y=np.round(vec.y - r*sin_t, decimals=9))
+
+    if ret_pts:
+        pts = vecs_to_gmsh_points([v1, v2], chip_z)
+        return pts
+    else:
+        return [v1, v2]
+
+def arc_width_offset_pts(vec1:Vec2D, vec3:Vec2D, angle:float, width:float, chip_z:float):
+    perp_angle = np.pi/2 - angle # π - (angle + π/2)
+    cos_t = np.round(np.cos(perp_angle), decimals=9)
+    sin_t = np.round(np.sin(perp_angle), decimals=9)
+    r = width/2
+
+    v1 = Vec2D(x=vec1.x, y=vec1.y+r)
+    v2 = Vec2D(x=vec1.x, y=vec1.y-r)
+
+    sign = 1 if angle == np.pi/2 else -1
+
+    v3 = Vec2D(x=np.round(vec3.x + (r*cos_t*sign), decimals=9),
+               y=np.round(vec3.y + (r*sin_t*sign), decimals=9))
+    v4 = Vec2D(x=np.round(vec3.x - (r*cos_t*sign), decimals=9),
+               y=np.round(vec3.y - (r*sin_t*sign), decimals=9))
+
+    pts = vecs_to_gmsh_points([v1, v2, v3, v4], chip_z)
+    return pts
+
+def make_arc_pts(angle:float, fillet:float):
+    sector_angle = np.pi - angle
+    intercept = np.abs(fillet * np.tan(sector_angle/2))
+    cos_t = np.round(np.cos(sector_angle), decimals=9)
+    sin_t = np.round(np.sin(sector_angle), decimals=9)
+
+    p1x = np.round(-intercept, decimals=9)
+    p1y = 0
+    p2x = np.round(p1x, decimals=9)
+    p2y = np.round(p1y + fillet, decimals=9)
+    p3x = np.round(intercept * cos_t, decimals=9)
+    p3y = np.round(intercept * sin_t, decimals=9)
+
+    v1 = Vec2D(x=p1x, y=p1y)
+    v2 = Vec2D(x=p2x, y=p2y)
+    v3 = Vec2D(x=p3x, y=p3y)
+
+    return v1, v2, v3
+
+def transform_arc_points(pts:list, translate:Vec2D, path_vecs:list, chip_z:float):
+    dim_tags = [(0, p) for p in pts]
+    new_pts = [p for p in pts]
+
+    angle1 = path_vecs[0].angle()
+    mirror = True if path_vecs[0].cross_prod_sign(path_vecs[1]) < 0 else False
+
+    if mirror:
+        # Flip right turn start/end points
+        p1 = new_pts.pop(0)
+        p4 = new_pts.pop(2)
+        new_pts.insert(1, p1)
+        new_pts.append(p4)
+        gmsh.model.occ.mirror(dim_tags, a=0, b=1, c=0, d=0)
+
+    gmsh.model.occ.rotate(dim_tags, x=0, y=0, z=0, ax=0, ay=0, az=1, angle=angle1)
+    gmsh.model.occ.translate(dim_tags, translate.x, translate.y, chip_z)
+    return new_pts
+
+def draw_curves(recent_pts:list, lines:list, arcs:list):
+    rec_pts = []
+    lns = []
+    acs = []
+
+    # TODO: think of a better error message
+    if len(recent_pts) not in [4, 7]:
+        raise RuntimeError("Unexpected geometry {len(recent_pts) not in [4,7]}. Check your geometry and retry.")
+
+    l1 = gmsh.model.occ.addLine(recent_pts[0], recent_pts[2])
+    l2 = gmsh.model.occ.addLine(recent_pts[1], recent_pts[3])
+    lns += [l1, l2]
+
+    if len(recent_pts) == 7:
+        a1 = gmsh.model.occ.addCircleArc(recent_pts[2], recent_pts[4], recent_pts[5])
+        a2 = gmsh.model.occ.addCircleArc(recent_pts[3], recent_pts[4], recent_pts[6])
+        acs += [a1, a2]
+
+    rec_pts = recent_pts[-2:]
+    lns += lines
+    acs += arcs
+
+    return rec_pts, lns, acs
+
+def render_path_curves(vecs: Vec2DArray, chip_z:float, fillet:float, width:float, straight_line_tol=1e-9):
+    recent_pts = [] # To store recent Gmsh points
+    lines = [] # To store Gmsh Lines
+    arcs = [] # to store Gmsh CircleArcs
+
+    for i,v in enumerate(vecs.points):
+        if i == 0:
+            p1, p2 = line_width_offset_pts(v, vecs.path_vecs[i], width, chip_z)
+            recent_pts += [p1, p2]
+            lines += [gmsh.model.occ.addLine(p1, p2)]
+            continue
+
+        if i == len(vecs.points)-1:
+            p1, p2 = line_width_offset_pts(v, vecs.path_vecs[i-1], width, chip_z)
+            recent_pts += [p1, p2]
+            lines += [gmsh.model.occ.addLine(p1, p2)]
+            continue
+
+        pv1 = vecs.path_vecs[i-1]; pv2 = vecs.path_vecs[i]
+        angle12 = vecs.get_angle_between(i-1, i)
+        if np.allclose(angle12, np.pi, rtol=straight_line_tol):
+            p1, p2 = line_width_offset_pts(v, pv2, width, chip_z)
+            recent_pts += [p1, p2]
+
+        else:
+            v1, v2, v3 = make_arc_pts(angle12, fillet)
+            p1, p2, p4, p5 = arc_width_offset_pts(v1, v3, angle12, width, chip_z)
+            p3 = vecs_to_gmsh_points([v2], chip_z)
+
+            new_pts = transform_arc_points([p1, p2, p3, p4, p5], v, [pv1, pv2], chip_z)
+            recent_pts += new_pts
+
+        recent_pts, lines, arcs = draw_curves(recent_pts, lines, arcs)
+
+    _, lines, arcs = draw_curves(recent_pts, lines, arcs)
+
+    return lines, arcs
