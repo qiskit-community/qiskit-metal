cabb62a82f5c9bb781d5d33a05c5d1a6dc602a1d
Priti Ashvin Shah <74020801+priti-ashvin-shah-ibm@users.noreply.github.com>
2022-07-27 14:01:38 -0400

    812 handler ls - allow for future usage for layers on a chip. (#813)
    * Provide a handler to read and give data from csv file for layer information.
    
    * Place a sample file for data into directory names resources. Change code so design planar and other designs can use the handler.
    
    * Change to default to include fill since z values are provided. Also prepare to change how the handler will work.
    
    * Need to add acceptable names for layer_stack file.
    
    * Add method for user to give list of properties they want. Still need to test this. Just saving work
    
    * Add general method.
    
    * Remove the methods which can be generalized.
    
    * Since we don't have to search with chip, we can remove from argument list.
    
    * Add chip_name as property for method, and remvoe method which sorts on chip_name.
    
    * The removed method which could be used by other method.
    
    * Place error message where both gmesh and other renderers can access it.

--- qiskit_metal/toolbox_metal/parsing.py
+++ qiskit_metal/toolbox_metal/parsing.py
@@ -177,6 +177,7 @@ from typing import Union
 import ast
 import numpy as np
 import pint
+from pint import UnitRegistry
 
 from .. import Dict, config, logger
 
@@ -196,6 +197,10 @@ __all__ = [
 TRUE_STR = [
     'true', 'True', 'TRUE', True, '1', 't', 'y', 'Y', 'YES', 'yes', 'yeah', 1,
     1.0
+]
+FALSE_STR = [
+    'false', 'False', 'FALSE', False, '0', 'f', 'n', 'N', 'NO', 'no', 'na', 0,
+    0.0
 ]
 
 
@@ -442,3 +447,90 @@ def parse_options(params: dict, parse_names: str, variable_dict=None):
         res += [parse_value(params[name], variable_dict)]
 
     return res
+
+
+##############################################################################
+# From pyepr, being used by renderer using comm port.
+#
+"""The methods in this section were copied from Ansys renderer which used comm-ports."""
+
+# UNITS
+# LENGTH_UNIT         --- HFSS UNITS
+# #Assumed default input units for ansys hfss
+LENGTH_UNIT = 'meter'
+# LENGTH_UNIT_ASSUMED --- USER UNITS
+# if a user inputs a blank number with no units in `parse_fix`,
+# we can assume the following using
+LENGTH_UNIT_ASSUMED = 'mm'
+
+try:
+    u_reg = UnitRegistry()
+    Q = u_reg.Quantity
+except (ImportError, ModuleNotFoundError):
+    pass  # raise NameError ("Pint module not installed. Please install.")
+
+
+def extract_value_unit(expr, units):
+    """
+    :type expr: str
+    :type units: str
+    :return: float
+    """
+    # pylint: disable=broad-except
+    try:
+        return Q(expr).to(units).magnitude
+    except Exception:
+        try:
+            return float(expr)
+        except Exception:
+            return expr
+
+
+def fix_units(x, unit_assumed=None):
+    '''
+    Convert all numbers to string and append the assumed units if needed.
+    For an iterable, returns a list
+    '''
+    unit_assumed = LENGTH_UNIT_ASSUMED if unit_assumed is None else unit_assumed
+    if isinstance(x, str):
+        # Check if there are already units defined, assume of form 2.46mm  or 2.0 or 4.
+        if x[-1].isdigit() or x[-1] == '.':  # number
+            return x + unit_assumed
+        else:  # units are already applied
+            return x
+
+    elif isinstance(x, Number):
+        return fix_units(str(x) + unit_assumed, unit_assumed=unit_assumed)
+
+    elif isinstance(x, Iterable):  # hasattr(x, '__iter__'):
+        return [fix_units(y, unit_assumed=unit_assumed) for y in x]
+    else:
+        return x
+
+
+def parse_entry(entry, convert_to_unit=LENGTH_UNIT):
+    '''
+    Should take a list of tuple of list... of int, float or str...
+    For iterables, returns lists
+    '''
+    if not isinstance(entry, list) and not isinstance(entry, tuple):
+        return extract_value_unit(entry, convert_to_unit)
+    else:
+        entries = entry
+        _entry = []
+        for entry in entries:
+            _entry.append(parse_entry(entry, convert_to_unit=convert_to_unit))
+        return _entry
+
+
+def parse_units(x):
+    '''
+    Convert number, string, and lists/arrays/tuples to numbers scaled
+    in HFSS units.
+
+    Converts to                  LENGTH_UNIT = meters  [HFSS UNITS]
+    Assumes input units  LENGTH_UNIT_ASSUMED = mm      [USER UNITS]
+
+    [USER UNITS] ----> [HFSS UNITS]
+    '''
+    return parse_entry(fix_units(x))
