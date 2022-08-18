791eaa1a71738c0f8b6f5709b952a902eee6457c
Nick Lanzillo <nalanzil@us.ibm.com>
2022-08-18 15:49:17 -0400

    updates to fix linting

--- qiskit_metal/qlibrary/core/design_check.py
+++ qiskit_metal/qlibrary/core/design_check.py
@@ -1,73 +1,72 @@
-### BASIC PROGRAM THAT LOOKS FOR OVERLAP BETWEEN QCOMPONENTS 
-from numpy import *
+### BASIC PROGRAM THAT LOOKS FOR OVERLAP BETWEEN QCOMPONENTS
+from numpy import size
 import geopandas as gpd
 import pandas as pd
 
-
-class QDesign_Check():
-    """QDesign_Check tests for overlap/collisions between qcomponents.
+class QDesignCheck():
+    """QDesign_Check contains various design checks.
     """
-    
-    def overlap_tester(design):
+
+    def overlap_tester(self):
+        design = self 
+        """This particular function tests for overlap between qcomponents
+        and CPWs.
+        """
 
         for unique_int in design._components:
-                print(" ")
-                print("Component ID:")
-                print(unique_int)
-                comp = unique_int
+            print(" ")
+            print("Component ID:")
+            print(unique_int)
+            comp = unique_int
 
-                # Get the GeoSeries tables separately for polys and paths, then combine.
-                poly = design._components[unique_int].qgeometry_table('poly')
-                path = design._components[unique_int].qgeometry_table('path')
-                combined = gpd.GeoDataFrame(pd.concat([poly, path], ignore_index=True))
-                poly_geo = poly["geometry"]
-                path_geo = path["geometry"]
-                combined_geo = combined["geometry"]
+            # Get the GeoSeries tables separately for polys and paths, then combine.
+            poly = design._components[unique_int].qgeometry_table('poly')
+            path = design._components[unique_int].qgeometry_table('path')
+            combined = gpd.GeoDataFrame(pd.concat([poly, path], ignore_index=True))
+            combined_geo = combined["geometry"]
 
-                # loop within a loop to calculate distance between components
-                for unique_int in design._components:
+            # loop within a loop to calculate distance between components
+            for unique_int in design._components:
 
-                    main_counter = 0.0
+                main_counter = 0.0
 
-                    # don't calculate distance between a component and itself
-                    if unique_int == comp:
-                        #print("Don't check for collisions between component and itself.")
-                        pass
-                    else:
-                        poly_inner = design._components[unique_int].qgeometry_table('poly')
-                        path_inner = design._components[unique_int].qgeometry_table('path')
-                        combined_inner = gpd.GeoDataFrame(pd.concat([poly_inner, path_inner], ignore_index=True))
-                        poly_geo_inner = poly_inner["geometry"]
-                        path_geo_inner = path_inner["geometry"]
-                        combined_geo_inner = combined_inner["geometry"]
+                # don't calculate distance between a component and itself
+                if unique_int == comp:
+                    #print("Don't check for collisions between component and itself.")
+                    pass
+                else:
+                    poly_inner = design._components[unique_int].qgeometry_table('poly')
+                    path_inner = design._components[unique_int].qgeometry_table('path')
+                    combined_inner = gpd.GeoDataFrame(pd.concat([poly_inner, path_inner],
+                                                                ignore_index=True))
+                    combined_geo_inner = combined_inner["geometry"]
 
-                        collision_counter_outer = 0.0
+                    collision_counter_outer = 0.0
 
-                        n = int(size(combined_geo_inner))
-                        for i in range(0,n):
+                    n = int(size(combined_geo_inner))
+                    for i in range(0,n):
 
-                            #print("inner element:", i)
-                            a=combined_geo.crosses(combined_geo_inner[i])
+                        #print("inner element:", i)
+                        el_inner=combined_geo.crosses(combined_geo_inner[i])
 
-                            m = int(size(a))
-                            collision_counter = 0.0
-                            for j in range(0,m):
-                                if a[j]==1:
-                                    collision_counter = collision_counter + 1.0
-                                else:
-                                    pass
-                            if collision_counter > 0.0:
-                                collision_counter_outer = collision_counter_outer + 1.0
-                                pass
+                        m = int(size(el_inner))
+                        collision_counter = 0.0
+                        for j in range(0,m):
+                            if el_inner[j]==1:
+                                collision_counter = collision_counter + 1.0
                             else:
                                 pass
-
-                        if collision_counter_outer > 0.0:
-                            main_counter = main_counter + 1.0
+                        if collision_counter > 0.0:
+                            collision_counter_outer = collision_counter_outer + 1.0
                         else:
                             pass
 
-                    if main_counter > 0.0:
-                        print("Has a collision with the following QComponent:", unique_int)
+                    if collision_counter_outer > 0.0:
+                        main_counter = main_counter + 1.0
                     else:
                         pass
+
+                if main_counter > 0.0:
+                    print("Has a collision with the following QComponent:", unique_int)
+                else:
+                    pass
