b427198299b2833200a2d2136dfd666229816ea8
Christina <cdelnano@gmail.com>
2022-10-26 13:42:52 -0400

    Added layer stack multiline string fix

--- qiskit_metal/tests/test_toolbox_metal.py
+++ qiskit_metal/tests/test_toolbox_metal.py
@@ -25,6 +25,7 @@ from typing import Union
 from qiskit_metal.toolbox_metal import about
 from qiskit_metal.toolbox_metal import parsing
 from qiskit_metal.toolbox_metal import math_and_overrides
+from qiskit_metal.toolbox_metal import bounds_for_path_and_poly_tables
 from qiskit_metal.toolbox_metal.bounds_for_path_and_poly_tables import BoundsForPathAndPolyTables
 from qiskit_metal.toolbox_metal.layer_stack_handler import LayerStackHandler
 from qiskit_metal.toolbox_metal.exceptions import QiskitMetalExceptions
@@ -358,9 +359,9 @@ class TestToolboxMetal(unittest.TestCase, AssertionsMixin):
 
     def test_toolbox_metal_get_bounds_of_path_and_poly_tables(self):
         """Test functionality of get_bounds_of_path_and_poly_tables in toolbox_metal.py"""
-        ls_file_path = """/Users/christinadelnano/Documents/
-                          qiskit-metal/tutorials/resources/
-                          flip_chip_layer_stack.txt"""
+        ls_file_path = ("/Users/christinadelnano/Documents/"
+                        "qiskit-metal/tutorials/resources/"
+                        "flip_chip_layer_stack.txt")
         multiplanar_design = MultiPlanar(metadata={},
                                          overwrite_enabled=True,
                                          layer_stack_filename=ls_file_path)
@@ -371,9 +372,9 @@ class TestToolboxMetal(unittest.TestCase, AssertionsMixin):
 
     def test_toolbox_metal_ensure_component_box_smaller_than_chip_box_(self):
         """Test functionality of ensure_component_box_smaller_than_chip_box in toolbox_metal.py"""
-        ls_file_path = """/Users/christinadelnano/Documents/
-                          qiskit-metal/tutorials/resources/
-                          flip_chip_layer_stack.txt"""
+        ls_file_path = ("/Users/christinadelnano/Documents/"
+                        "qiskit-metal/tutorials/resources/"
+                        "flip_chip_layer_stack.txt")
         multiplanar_design = MultiPlanar(metadata={},
                                          overwrite_enabled=True,
                                          layer_stack_filename=ls_file_path)
@@ -400,9 +401,9 @@ class TestToolboxMetal(unittest.TestCase, AssertionsMixin):
 
     def test_toolbox_metal_get_box_for_xy_bounds(self):
         """Test functionality of get_box_for_xy_bounds in toolbox_metal.py."""
-        ls_file_path = """/Users/christinadelnano/Documents/
-                          qiskit-metal/tutorials/resources/
-                          flip_chip_layer_stack.txt"""
+        ls_file_path = ("/Users/christinadelnano/Documents/"
+                        "qiskit-metal/tutorials/resources/"
+                        "flip_chip_layer_stack.txt")
         multiplanar_design = MultiPlanar(metadata={},
                                          overwrite_enabled=True,
                                          layer_stack_filename=ls_file_path)
@@ -429,9 +430,9 @@ class TestToolboxMetal(unittest.TestCase, AssertionsMixin):
 
     def test_toolbox_metal_are_all_chipnames_in_design(self):
         """Test functionality of are_all_chipnames_in_design in toolbox_metal.py."""
-        ls_file_path = """/Users/christinadelnano/Documents/
-                          qiskit-metal/tutorials/resources/
-                          flip_chip_layer_stack.txt"""
+        ls_file_path = ("/Users/christinadelnano/Documents/"
+                        "qiskit-metal/tutorials/resources/"
+                        "flip_chip_layer_stack.txt")
         multiplanar_design = MultiPlanar(metadata={},
                                          overwrite_enabled=True,
                                          layer_stack_filename=ls_file_path)
@@ -458,9 +459,9 @@ class TestToolboxMetal(unittest.TestCase, AssertionsMixin):
 
     def test_toolbox_metal_get_x_y_for_chip(self):
         """Test functionality of get_x_y_for_chip in toolbox_metal.py."""
-        ls_file_path = """/Users/christinadelnano/Documents/
-                          qiskit-metal/tutorials/resources/
-                          flip_chip_layer_stack.txt"""
+        ls_file_path = ("/Users/christinadelnano/Documents/"
+                        "qiskit-metal/tutorials/resources/"
+                        "flip_chip_layer_stack.txt")
         multiplanar_design = MultiPlanar(metadata={},
                                          overwrite_enabled=True,
                                          layer_stack_filename=ls_file_path)
@@ -489,9 +490,9 @@ class TestToolboxMetal(unittest.TestCase, AssertionsMixin):
 
     def test_toolbox_metal_chip_names_not_in_design(self):
         """Test functionality of chip_names_not_in_design in toolbox_metal.py."""
-        ls_file_path = """/Users/christinadelnano/Documents/
-                          qiskit-metal/tutorials/resources/
-                          flip_chip_layer_stack.txt"""
+        ls_file_path = ("/Users/christinadelnano/Documents/"
+                        "qiskit-metal/tutorials/resources/"
+                        "flip_chip_layer_stack.txt")
         multiplanar_design = MultiPlanar(metadata={},
                                          overwrite_enabled=True,
                                          layer_stack_filename=ls_file_path)
@@ -503,9 +504,9 @@ class TestToolboxMetal(unittest.TestCase, AssertionsMixin):
 
     def test_toolbox_metal_chip_size_not_in_chipname_within_design(self):
         """Test functionality of chip_size_not_in_chipname_within_design in toolbox_metal.py."""
-        ls_file_path = """/Users/christinadelnano/Documents/
-                          qiskit-metal/tutorials/resources/
-                          flip_chip_layer_stack.txt"""
+        ls_file_path = ("/Users/christinadelnano/Documents/"
+                        "qiskit-metal/tutorials/resources/"
+                        "flip_chip_layer_stack.txt")
         multiplanar_design = MultiPlanar(metadata={},
                                          overwrite_enabled=True,
                                          layer_stack_filename=ls_file_path)
@@ -515,9 +516,9 @@ class TestToolboxMetal(unittest.TestCase, AssertionsMixin):
 
     def test_toolbox_metal_get_layer_datatype_when_fill_is_true(self):
         """Test functionality of get_layer_datatype_when_fill_is_true in toolbox_metal.py."""
-        ls_file_path = """/Users/christinadelnano/Documents/
-                          qiskit-metal/tutorials/resources/
-                          flip_chip_layer_stack.txt"""
+        ls_file_path = ("/Users/christinadelnano/Documents/"
+                        "qiskit-metal/tutorials/resources/"
+                        "flip_chip_layer_stack.txt")
         multiplanar_design = MultiPlanar(metadata={},
                                          overwrite_enabled=True,
                                          layer_stack_filename=ls_file_path)
@@ -561,9 +562,9 @@ class TestToolboxMetal(unittest.TestCase, AssertionsMixin):
 
     def test_toolbox_metal_get_properties_for_layer_datatype(self):
         """Test functionality of get_properties_for_layer_datatype in toolbox_metal.py."""
-        ls_file_path = """/Users/christinadelnano/Documents/
-                          qiskit-metal/tutorials/resources/
-                          flip_chip_layer_stack.txt"""
+        ls_file_path = ("/Users/christinadelnano/Documents/"
+                        "qiskit-metal/tutorials/resources/"
+                        "flip_chip_layer_stack.txt")
         multiplanar_design = MultiPlanar(metadata={},
                                          overwrite_enabled=True,
                                          layer_stack_filename=ls_file_path)
@@ -580,9 +581,9 @@ class TestToolboxMetal(unittest.TestCase, AssertionsMixin):
 
     def test_toolbox_metal_is_layer_data_unique(self):
         """Test functionality of is_layer_data_unique in toolbox_metal.py."""
-        ls_file_path = """/Users/christinadelnano/Documents/
-                          qiskit-metal/tutorials/resources/
-                          flip_chip_layer_stack.txt"""
+        ls_file_path = ("/Users/christinadelnano/Documents/"
+                        "qiskit-metal/tutorials/resources/"
+                        "flip_chip_layer_stack.txt")
         multiplanar_design = MultiPlanar(metadata={},
                                          overwrite_enabled=True,
                                          layer_stack_filename=ls_file_path)
@@ -593,9 +594,9 @@ class TestToolboxMetal(unittest.TestCase, AssertionsMixin):
 
     def test_toolbox_metal_read_csv_df(self):
         """Test functionality of read_csv_df in toolbox_metal.py."""
-        ls_file_path = """/Users/christinadelnano/Documents/
-                          qiskit-metal/tutorials/resources/
-                          flip_chip_layer_stack.txt"""
+        ls_file_path = ("/Users/christinadelnano/Documents/"
+                        "qiskit-metal/tutorials/resources/"
+                        "flip_chip_layer_stack.txt")
         multiplanar_design = MultiPlanar(metadata={},
                                          overwrite_enabled=True,
                                          layer_stack_filename=ls_file_path)
@@ -606,9 +607,9 @@ class TestToolboxMetal(unittest.TestCase, AssertionsMixin):
 
     def test_toolbox_metal_get_unique_chip_names(self):
         """Test functionality of get_unique_chip_names in toolbox_metal.py."""
-        ls_file_path = """/Users/christinadelnano/Documents/
-                          qiskit-metal/tutorials/resources/
-                          flip_chip_layer_stack.txt"""
+        ls_file_path = ("/Users/christinadelnano/Documents/"
+                        "qiskit-metal/tutorials/resources/"
+                        "flip_chip_layer_stack.txt")
         multiplanar_design = MultiPlanar(metadata={},
                                          overwrite_enabled=True,
                                          layer_stack_filename=ls_file_path)
@@ -619,9 +620,9 @@ class TestToolboxMetal(unittest.TestCase, AssertionsMixin):
 
     def test_toolbox_metal_get_unique_layer_ints(self):
         """Test functionality of get_unique_layer_ints in toolbox_metal.py."""
-        ls_file_path = """/Users/christinadelnano/Documents/
-                          qiskit-metal/tutorials/resources/
-                          flip_chip_layer_stack.txt"""
+        ls_file_path = ("/Users/christinadelnano/Documents/"
+                        "qiskit-metal/tutorials/resources/"
+                        "flip_chip_layer_stack.txt")
         multiplanar_design = MultiPlanar(metadata={},
                                          overwrite_enabled=True,
                                          layer_stack_filename=ls_file_path)
@@ -632,9 +633,9 @@ class TestToolboxMetal(unittest.TestCase, AssertionsMixin):
 
     def test_toolbox_metal_warning_properties(self):
         """Test functionality of _warning_properties in toolbox_metal.py."""
-        ls_file_path = """/Users/christinadelnano/Documents/
-                          qiskit-metal/tutorials/resources/
-                          flip_chip_layer_stack.txt"""
+        ls_file_path = ("/Users/christinadelnano/Documents/"
+                        "qiskit-metal/tutorials/resources/"
+                        "flip_chip_layer_stack.txt")
         multiplanar_design = MultiPlanar(metadata={},
                                          overwrite_enabled=True,
                                          layer_stack_filename=ls_file_path)
@@ -645,9 +646,9 @@ class TestToolboxMetal(unittest.TestCase, AssertionsMixin):
 
     def test_toolbox_metal_warning_search(self):
         """Test functionality of _warning_search in toolbox_metal.py."""
-        ls_file_path = """/Users/christinadelnano/Documents/
-                          qiskit-metal/tutorials/resources/
-                          flip_chip_layer_stack.txt"""
+        ls_file_path = ("/Users/christinadelnano/Documents/"
+                        "qiskit-metal/tutorials/resources/"
+                        "flip_chip_layer_stack.txt")
         multiplanar_design = MultiPlanar(metadata={},
                                          overwrite_enabled=True,
                                          layer_stack_filename=ls_file_path)
@@ -658,9 +659,9 @@ class TestToolboxMetal(unittest.TestCase, AssertionsMixin):
 
     def test_toolbox_metal_warning_search_minus_chip(self):
         """Test functionality of _warning_search_minus_chip in toolbox_metal.py."""
-        ls_file_path = """/Users/christinadelnano/Documents/
-                          qiskit-metal/tutorials/resources/
-                          flip_chip_layer_stack.txt"""
+        ls_file_path = ("/Users/christinadelnano/Documents/"
+                        "qiskit-metal/tutorials/resources/"
+                        "flip_chip_layer_stack.txt")
         multiplanar_design = MultiPlanar(metadata={},
                                          overwrite_enabled=True,
                                          layer_stack_filename=ls_file_path)
@@ -671,9 +672,9 @@ class TestToolboxMetal(unittest.TestCase, AssertionsMixin):
 
     def test_toolbox_metal_layer_stack_handler_pilot_error(self):
         """Test functionality of layer_stack_handler_pilot_error in toolbox_metal.py."""
-        ls_file_path = """/Users/christinadelnano/Documents/
-                          qiskit-metal/tutorials/resources/
-                          flip_chip_layer_stack.txt"""
+        ls_file_path = ("/Users/christinadelnano/Documents/"
+                        "qiskit-metal/tutorials/resources/"
+                        "flip_chip_layer_stack.txt")
         multiplanar_design = MultiPlanar(metadata={},
                                          overwrite_enabled=True,
                                          layer_stack_filename=ls_file_path)
