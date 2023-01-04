ae54e24b1354bebc7a3d57fecc58621c777647c2
AbeerVaishnav13 <abeer.vaishnav13@gmail.com>
2023-01-04 20:09:53 +0530

    Add docstrings and fix imports

--- qiskit_metal/renderers/renderer_elmer/elmer_runner.py
+++ qiskit_metal/renderers/renderer_elmer/elmer_runner.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-import typing as ty
+from typing import Union, Any
 import os
 import subprocess
 import shutil
@@ -7,30 +7,49 @@ from collections import defaultdict
 import platform
 import yaml
 
-from ... import Dict
-from .elmer_configs import materials, simulations, solvers
+from qiskit_metal import Dict
+from qiskit_metal.renderers.renderer_elmer.elmer_configs import materials, simulations, solvers
 
 
 @dataclass
 class Body:
-    target_bodies: ty.List[int]
+    """ Class for storing data for ElmerFEM Body """
+    target_bodies: list[int]
     equation: int
     material: int
-    body_force: ty.Union[int, None]
-    initial_condition: ty.Union[int, None]
+    body_force: Union[int, None]
+    initial_condition: Union[int, None]
 
 
 @dataclass
 class Boundary:
-    target_boundaries: ty.List[int]
+    """ Class for storing data for ElmerFEM boundary """
+    target_boundaries: list[int]
     other_opts: dict
 
 
 class ElmerRunner:
-    """TODO: complete class docstring
+    """Class for generating the Simulation Input File (SIF) for ElmerFEM to run the simulation.
+
+    Attributes: 
+        header: Header information for the SIF
+        simulation: Simulation information for the SIF
+        constant: Constants for the SIF
+        solver: Solver information for the SIF
+        equation: Equation information for the SIF
+        material: Material information for the SIF
+        body_force: Body force information for the SIF
+        initial_condition: Initial condition information for the SIF
+        body: Body information for the SIF
+        boundary_condition: Boundary condition information for the SIF
     """
 
     def __init__(self) -> None:
+        """Initialize the ElmerRunner object
+
+        Args:
+            self: reference to self object
+        """
         self.header = 'Header\n  CHECK KEYWORDS "Warn"\n  Mesh DB "." "."\nEnd\n\n'
         self.simulation = defaultdict(dict)
         self.constant = dict()
@@ -43,85 +62,215 @@ class ElmerRunner:
         self.boundary_condition = defaultdict(Boundary)
 
     @property
-    def solver_info(self):
+    def solver_info(self) -> list[str]:
+        """Display the types of solvers used in the simulation.
+
+        Returns:
+            list[str]: returns the list of solver names
+        """
         return list(self.solver.keys())
 
     @property
-    def simulation_info(self):
+    def simulation_info(self) -> list[str]:
+        """Display the types of simulation used in the SIF.
+
+        Returns:
+            list[str]: returns the list of simulation names
+        """
         return list(self.simulation.keys())
 
     @property
-    def equation_info(self):
+    def equation_info(self) -> list[str]:
+        """Display the types of equations used in the simulation.
+
+        Returns:
+            list[str]: returns the list of equation names
+        """
         return list(self.equation.keys())
 
     def add_setup_options(
         self,
         setup_param: Dict,
-        new_opt: ty.Dict[str, ty.Any],
+        new_opt: dict[str, Any],
     ):
+        """Function to modify the setup options using the ones
+        provided by the user in QElmerRenderer.
+
+        Args:
+            setup_param (Dict): Setup parameters to be written in SIF.
+            new_opt (dict[str, Any]): user-modified parameters from the
+                                        QElmerRenderer setup.
+        """
         modified_new_opt = {k.replace(' ', '_'): v for k, v in new_opt.items()}
         setup_param.update(modified_new_opt)
 
     def init_simulation(self, name: str, options: dict):
+        """Function to initialize a simulation.
+
+        Args:
+            name (str): Simulation name.
+            options (dict): Simulation options.
+        """
         if len(self.simulation.keys()) > 1:
             raise RuntimeError(
                 "There can only be one simulation instance at a time.")
 
         self.simulation.update({name: options})
 
-    def add_equation(self, name: str, solvers: list):
+    def add_equation(self, name: str, solvers: list) -> int:
+        """Function to add an equation to the current simulation.
+
+        Args:
+            name (str): Equation name.
+            solvers (list): Solvers corresponding to each equation.
+
+        Returns:
+            int: Equation ID in the simulation.
+        """
         self.equation.update({name: solvers})
         return len(self.equation)
 
     def add_constant(self, name: str, value: str):
+        """Function to add a constant to the simulation.
+
+        Args:
+            name (str): Constant name.
+            value (str): Value of the constant in appropriate units.
+        """
         self.constant.update({name: value})
 
-    def add_solver(self, name: str, options: dict):
+    def add_solver(self, name: str, options: dict) -> int:
+        """Function to add a solver to the simulation.
+
+        Args:
+            name (str): Solver name.
+            options (dict): Solver options.
+
+        Returns:
+            int: Solver ID in the simulation.
+        """
         self.solver.update({name: options})
         return len(self.solver)
 
-    def add_material(self, name: str, options: dict):
+    def add_material(self, name: str, options: dict) -> int:
+        """Function to add a material in the simulation.
+
+        Args:
+            name (str): Material name.
+            options (dict): Material options.
+
+        Returns:
+            int: Material ID in the simulation.
+        """
         self.material.update({name: options})
         return len(self.material)
 
     def add_body(self,
                  name: str,
-                 target_bodies: ty.List[int],
+                 target_bodies: list[int],
                  equation: int,
                  material: int,
-                 body_force: ty.Union[int, None] = None,
-                 initial_condition: ty.Union[int, None] = None):
+                 body_force: Union[int, None] = None,
+                 initial_condition: Union[int, None] = None) -> int:
+        """Function to add a Body in the simulation.
+
+        Args:
+            name (str): Body name.
+            target_bodies (list[int]): Target ID of volume in the mesh.
+            equation (int): Equation associated with the body.
+            material (int): Material associated with the body.
+            body_force (Union[int, None]): Body force associated with the body.
+            initial_condition (Union[int, None]): Initial condition associated with the body.
+
+        Returns:
+            int: Body ID in the simulation.
+        """
         sim_body = Body(target_bodies, equation, material, body_force,
                         initial_condition)
         self.body.update({name: sim_body})
         return len(self.body)
 
-    def add_body_force(self, name: str, options: dict):
+    def add_body_force(self, name: str, options: dict) -> int:
+        """Function to add a body force in the simulation.
+
+        Args:
+            name (str): Body force name.
+            options (dict): Body force options.
+
+        Returns:
+            int: Body force ID in the simulation.
+        """
         self.body_force.update({name: options})
         return len(self.material)
 
-    def add_boundary_condition(self, name: str, target_boundaries: ty.List[int],
-                               options: dict):
+    def add_boundary_condition(self, name: str, target_boundaries: list[int],
+                               options: dict) -> int:
+        """Function to add a boundary condition in the simulation.
+
+        Args:
+            name (str): Boundary condition name.
+            target_boundaries (list[int]): Target ID of surface/curve in the mesh.
+            options (dict): Boundary condition options.
+
+        Returns:
+            int: Boundary condition ID in the simulation.
+        """
         sim_boundary = Boundary(target_boundaries, options)
         self.boundary_condition.update({name: sim_boundary})
         return len(self.boundary_condition)
 
-    def add_initial_condition(self, name: str, options: dict):
+    def add_initial_condition(self, name: str, options: dict) -> int:
+        """Function to add an initial condition in the simulation.
+
+        Args:
+            name (str): Initial condition name.
+            options (dict): Initial condition options.
+
+        Returns:
+            int: Initial condition ID in the simulation.
+        """
         self.initial_condition.update({name: options})
         return len(self.initial_condition)
 
     def _update_solver_setup(self, yaml_setup_dict: dict,
-                             new_setup_params: dict):
+                             new_setup_params: dict) -> dict:
+        """Function to update the solver setup from QElmerRenderer
+        default setup.
+
+        Args:
+            yaml_setup_dict (dict): setup dictionary from the YAML file.
+            new_setup_params (dict): User-modified setup parameters
+                                        in QElmerRenderer.
+
+        Returns:
+            dict: Modifed YAML setup dictionary
+        """
         yaml_setup_dict.update(new_setup_params)
         return yaml_setup_dict
 
-    def _read_yaml_to_dict(self, filename: str):
+    def _read_yaml_to_dict(self, filename: str) -> dict:
+        """Function to read a YAML setup file for simulation ad solver details.
+
+        Args:
+            filename (str): file for the YAML setup.
+
+        Returns:
+            dict: Returns dictionary populated using the YAML file.
+        """
         with open(filename, "r", encoding="utf-8") as f:
             yaml_setup = yaml.safe_load(f)
             f.close()
             return yaml_setup
 
     def load_simulation(self, name: str, config_file: str = None):
+        """Function to load the simulation.
+
+        Args:
+            name (str): Simulation name.
+            config_file (str): Optional YAML configuration file
+                                to load the simulation from.
+                                Defaults to None.
+        """
         if config_file is not None:
             opts = self._read_yaml_to_dict(config_file)
         else:
@@ -133,7 +282,20 @@ class ElmerRunner:
                 "to manually add new simulation to the configuration.")
         self.init_simulation(name=name, options=opts[name])
 
-    def load_solvers(self, solver_names: ty.List[str], config_file: str = None):
+    def load_solvers(self,
+                     solver_names: list[str],
+                     config_file: str = None) -> list[int]:
+        """Function to load the solvers (from YAML configutation if specidifed).
+
+        Args:
+            solver_names (list[str]): Solver names.
+            config_file (str): Optional YAML configuration file
+                                to load the simulation from.
+                                Defaults to None.
+
+        Returns:
+            list[int]: Returns a list of solvers that are loaded and active.
+        """
         if config_file is not None:
             opts = self._read_yaml_to_dict(config_file)
         else:
@@ -149,9 +311,19 @@ class ElmerRunner:
         return [i + 1 for i in range(len(self.solver))]
 
     def load_materials(self,
-                       material_names: ty.List[str],
-                       config_file: str = None):
+                       material_names: list[str],
+                       config_file: str = None) -> list[int]:
+        """Function to load the materials (from YAML configuration if specified).
 
+        Args:
+            material_names (list[str]): Material names.
+            config_file (str): Optional YAML configuration file
+                                to load the simulation from.
+                                Defaults to None.
+
+        Returns:
+            list[int]: Returns a list of materials that are loaded and active.
+        """
         if config_file is not None:
             opts = self._read_yaml_to_dict(config_file)
         else:
@@ -166,7 +338,13 @@ class ElmerRunner:
 
         return [i + 1 for i in range(len(self.material))]
 
-    def _write_sif_simulation(self):
+    def _write_sif_simulation(self) -> str:
+        """Write the simulation in SIF using data stored
+        as a single Python string.
+
+        Returns:
+            str: Returns the partially written string for SIF.
+        """
         section_string = ""
         if len(self.simulation) > 0:
             section_string += f"! {list(self.simulation.keys())[0]}\n"
@@ -176,7 +354,13 @@ class ElmerRunner:
             section_string += "End\n\n"
         return section_string
 
-    def _write_sif_constants(self):
+    def _write_sif_constants(self) -> str:
+        """Write the constants in SIF using data stored
+        as a single Python string.
+
+        Returns:
+            str: Returns the partially written string for SIF.
+        """
         section_string = ""
         if len(self.constant) > 0:
             section_string += "Constants\n"
@@ -185,7 +369,13 @@ class ElmerRunner:
             section_string += "End\n\n"
         return section_string
 
-    def _write_sif_equation(self):
+    def _write_sif_equation(self) -> str:
+        """Write the equations in SIF using data stored
+        as a single Python string.
+
+        Returns:
+            str: Returns the partially written string for SIF.
+        """
         section_string = ""
         if len(self.equation) > 0:
             for i, k in enumerate(self.equation):
@@ -204,7 +394,13 @@ class ElmerRunner:
             section_string += '\n'
         return section_string
 
-    def _write_sif_solver(self):
+    def _write_sif_solver(self) -> str:
+        """Write the solvers in SIF using data stored
+        as a single Python string.
+
+        Returns:
+            str: Returns the partially written string for SIF.
+        """
         section_string = ""
         if len(self.solver) > 0:
             for i, k in enumerate(self.solver):
@@ -216,7 +412,13 @@ class ElmerRunner:
             section_string += '\n'
         return section_string
 
-    def _write_sif_material(self):
+    def _write_sif_material(self) -> str:
+        """Write the materials in SIF using data stored
+        as a single Python string.
+
+        Returns:
+            str: Returns the partially written string for SIF.
+        """
         section_string = ""
         if len(self.material) > 0:
             for i, k in enumerate(self.material):
@@ -228,7 +430,13 @@ class ElmerRunner:
             section_string += '\n'
         return section_string
 
-    def _write_sif_body(self):
+    def _write_sif_body(self) -> str:
+        """Write the bodies in SIF using data stored
+        as a single Python string.
+
+        Returns:
+            str: Returns the partially written string for SIF.
+        """
         section_string = ""
         if len(self.body) > 0:
             for i, k in enumerate(self.body):
@@ -255,7 +463,13 @@ class ElmerRunner:
             section_string += '\n'
         return section_string
 
-    def _write_sif_boundary_condition(self):
+    def _write_sif_boundary_condition(self) -> str:
+        """Write the boundary conditions in SIF using data stored
+        as a single Python string.
+
+        Returns:
+            str: Returns the partially written string for SIF.
+        """
         section_string = ""
         if len(self.boundary_condition) > 0:
             for i, k in enumerate(self.boundary_condition):
@@ -272,7 +486,13 @@ class ElmerRunner:
             section_string += '\n'
         return section_string
 
-    def _write_sif_initial_condition(self):
+    def _write_sif_initial_condition(self) -> str:
+        """Write the initial conditions in SIF using data stored
+        as a single Python string.
+
+        Returns:
+            str: Returns the partially written string for SIF.
+        """
         section_string = ""
         if len(self.initial_condition) > 0:
             for i, k in enumerate(self.initial_condition):
@@ -285,6 +505,13 @@ class ElmerRunner:
         return section_string
 
     def write_startinfo_and_sif(self, filename: str, sim_dir: str):
+        """Write the SIF and STARTINFO files for ElmerFEM to
+        provide the simulation setup.
+
+        Args:
+            filename (str): filename for the SIF.
+            sim_dir (str): Directory for storing all the simulation data.
+        """
         if not os.path.exists(sim_dir):
             os.mkdir(sim_dir)
 
@@ -313,7 +540,16 @@ class ElmerRunner:
                       meshfile: str,
                       elmergrid: str = None,
                       options: list = []):
+        """Function to run ElmerGrid executable in the system for transforming
+        Gmsh style mesh to ElmerFEM style mesh.
 
+        Args:
+            sim_dir (str): Directory for storing simulation data.
+            meshfile (str): Gmsh mesh file
+            elmergrid (str): Path to ElmerGrid executable (if not installed
+                                using the default procedure). Defaults to None.
+            options (list): ElmerGrid additional options.
+        """
         os_platform = platform.system()
         if elmergrid is None:
             # On Windows ElmerGrid.exe is not found once gmsh.initialize() was executed.
@@ -349,7 +585,16 @@ class ElmerRunner:
             # out_files: ty.List[str],
             elmersolver: str = None,
             options: list = []):
+        """Function to run ElmerSolver executable in the system
+        running the FEM simulation.
 
+        Args:
+            sim_dir (str): Directory for storing simulation data.
+            sif_file (str): Simulation Input File (SIF) path
+            elmersolver (str): Path to ElmerSolver executable (if not installed
+                                using the default procedure). Defaults to None.
+            options (list): ElmerSolver additional options.
+        """
         os_platform = platform.system()
         if elmersolver is None:
             # On Windows ElmerSolver.exe is not found once gmsh.initialize() was executed.
