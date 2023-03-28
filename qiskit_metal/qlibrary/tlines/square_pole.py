adf7025f51257b5288aead99c24df96fd2894295
clarkmiyamoto <cmiyamot@usc.edu>
2023-03-28 12:03:10 -0700

    fixed yapf formatting

--- qiskit_metal/qlibrary/tlines/square_pole.py
+++ qiskit_metal/qlibrary/tlines/square_pole.py
@@ -1,26 +1,53 @@
+# -*- coding: utf-8 -*-
+
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2017, 2023.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
 from qiskit_metal import draw, Dict
 from qiskit_metal.qlibrary.core import QComponent
 
+
 class SPole(QComponent):
-    
-    default_options = Dict(width = 'cpw_width',
-                           gap = 'cpw_gap')
-    
+    """
+    Implements square CPW in the +z axis. Used in MultiPlanar designs.
+
+Inherits `QComponent` class
+
+    Default Options:
+        * width: 'cpw_width' -- Width of square, defaults to 'cpw_width'.
+        * gap: 'cpw_gap' -- Gap between CPW and grounding plane, defaults to 'cpw_gap'.
+    """
+
+    default_options = Dict(width='cpw_width', gap='cpw_gap')
+    """Default options"""
+
+    TOOLTIP = "Implements a straight CPW in the z axis."
+
     def make(self):
         p = self.p
-        self.options.pos_x, self.options.pos_y =self.get_pin_location(p.name, p.pin)
+        self.options.pos_x, self.options.pos_y = self.get_pin_location(
+            p.name, p.pin)
 
         # Draw SPole
         pole = draw.rectangle(p.width, p.width, 0, 0)
-        pole_etch = draw.rectangle(p.width + 2 * p.gap, p.width + 2 * p.gap, 0, 0)
-        
+        pole_etch = draw.rectangle(p.width + 2 * p.gap, p.width + 2 * p.gap, 0,
+                                   0)
+
         # Rotate and Translate
         polys = [pole, pole_etch]
         polys = draw.rotate(polys, p.orientation, origin=(0, 0))
         polys = draw.translate(polys, self.options.pos_x, self.options.pos_y)
-        
         [pole, pole_etch] = polys
-        
+
         for layer in p.layers:
             self.add_qgeometry('poly', {'pole': pole},
                                subtract=False,
@@ -30,8 +57,7 @@ class SPole(QComponent):
                                subtract=True,
                                layer=layer,
                                chip=p.chip)
-            
-    def get_pin_location(self, name, pin):
+
+    def get_pin_location(self, name: str, pin: str) -> float:
         pos = self.design.components[name].pins[pin].middle
         return pos
-
