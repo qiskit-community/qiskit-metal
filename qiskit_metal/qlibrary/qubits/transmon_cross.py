556eadefe0108802a76abed4d860c9f6c59a4478
clarkmiyamoto <cmiyamot@usc.edu>
2023-06-21 23:39:21 -1000

    fixed header on transmon_cross.py

--- qiskit_metal/qlibrary/qubits/transmon_cross.py
+++ qiskit_metal/qlibrary/qubits/transmon_cross.py
@@ -17,25 +17,6 @@ from qiskit_metal import draw, Dict
 from qiskit_metal.qlibrary.core import BaseQubit
 
 
-# -*- coding: utf-8 -*-
-
-# This code is part of Qiskit.
-#
-# (C) Copyright IBM 2017, 2021.
-#
-# This code is licensed under the Apache License, Version 2.0. You may
-# obtain a copy of this license in the LICENSE.txt file in the root directory
-# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
-#
-# Any modifications or derivative works of this code must retain this
-# copyright notice, and modified files need to carry a notice indicating
-# that they have been altered from the originals.
-
-import numpy as np
-from qiskit_metal import draw, Dict
-from qiskit_metal.qlibrary.core import BaseQubit
-
-
 class TransmonCross(BaseQubit):  # pylint: disable=invalid-name
     """The base `TransmonCross` class.
 
