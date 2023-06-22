123de0a0fd5aaad7eac8ccdbf13240ffc96573b4
clarkmiyamoto <cmiyamot@usc.edu>
2023-06-21 23:54:25 -1000

    fixed indentation error in test

--- qiskit_metal/tests/test_qlibrary_2_options.py
+++ qiskit_metal/tests/test_qlibrary_2_options.py
@@ -496,8 +496,8 @@ class TestComponentOptions(unittest.TestCase, AssertionsMixin):
                          '5um')
         self.assertEqual(
             _options['_default_connection_pads']['ground_spacing_back'], None)
-         self.assertEqual(_options['_default_connection_pads']['claw_width'],
-                          '10um')
+        self.assertEqual(_options['_default_connection_pads']['claw_width'],
+                         '10um')
         self.assertEqual(
             _options['_default_connection_pads']['claw_width_back'], None)
         self.assertEqual(_options['_default_connection_pads']['claw_gap'],
