b1d75e5ad25c7fdc184514c00bd407961c6a9352
clarkmiyamoto <cmiyamot@usc.edu>
2023-06-26 16:49:07 -1000

    correct pathing for Airbridge_forGDS

--- qiskit_metal/renderers/renderer_gds/gds_renderer.py
+++ qiskit_metal/renderers/renderer_gds/gds_renderer.py
@@ -33,7 +33,7 @@ import pandas as pd
 import numpy as np
 
 from qiskit_metal.renderers.renderer_base import QRenderer
-from airbridge import Airbridge_forGDS
+from qiskit_metal.renderers.renderer_gds.airbridge import Airbridge_forGDS
 from qiskit_metal.renderers.renderer_gds.make_cheese import Cheesing
 from qiskit_metal.toolbox_metal.parsing import is_true
 from qiskit_metal import draw
