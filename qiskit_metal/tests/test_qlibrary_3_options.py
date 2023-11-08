c112ecf7828e90a6644670cef98bedf8044babd7
Arnau Casau <arnaucasau@gmail.com>
2023-11-08 08:57:26 +0100

    Fix unit test

--- qiskit_metal/tests/test_qlibrary_3_options.py
+++ qiskit_metal/tests/test_qlibrary_3_options.py
@@ -288,7 +288,7 @@ class TestComponentOptions(unittest.TestCase, AssertionsMixin):
             design, 'my_name')
         options = my_transmon_concentric.default_options
 
-        self.assertEqual(len(options), 16)
+        self.assertEqual(len(options), 17)
         self.assertEqual(options['width'], '1000um')
         self.assertEqual(options['height'], '1000um')
         self.assertEqual(options['rad_o'], '170um')
@@ -487,7 +487,7 @@ class TestComponentOptions(unittest.TestCase, AssertionsMixin):
         self.assertEqual(_options['cross_length'], '200um')
         self.assertEqual(_options['cross_gap'], '20um')
 
-        self.assertEqual(len(_options['_default_connection_pads']), 6)
+        self.assertEqual(len(_options['_default_connection_pads']), 8)
         self.assertEqual(_options['_default_connection_pads']['connector_type'],
                          '0')
         self.assertEqual(_options['_default_connection_pads']['claw_length'],
@@ -496,6 +496,10 @@ class TestComponentOptions(unittest.TestCase, AssertionsMixin):
                          '5um')
         self.assertEqual(_options['_default_connection_pads']['claw_width'],
                          '10um')
+        self.assertEqual(_options['_default_connection_pads']['claw_cpw_length'],
+                    '40um')
+        self.assertEqual(_options['_default_connection_pads']['claw_cpw_width'],
+                    '10um')
         self.assertEqual(_options['_default_connection_pads']['claw_gap'],
                          '6um')
         self.assertEqual(
